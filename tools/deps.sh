c15f0d6146d858d07819cecd342b6d3834d494bc
philsmd <philsmd@hashcat.net>
2015-12-09 15:55:06 +0100

    issue #16: use just "deps" instead of "hashcat-deps" as directory name

--- tools/deps.sh
+++ tools/deps.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 # Author: Gabriele Gristina <matrix@hashcat.net>
-# Revision: 1.01
+# Revision: 1.02
 
 ## global vars
 DEPS="make gcc g++ gcc-multilib g++-multilib libc6-dev-i386 mingw-w64 build-essential unzip"
 DOWNLOAD_DEPS="ADL_SDK8.zip R352-developer.zip cuda_7.5.18_linux.run NVIDIA-Linux-x86_64-352.21.run gdk_linux_amd64_352_55_release.run AMD-APP-SDKInstaller-v3.0.130.135-GA-linux64.tar.bz2"
 
-## enter the hashcat-deps directory
+## enter the deps directory
 cur_directory=$(dirname ${0})
 script_directory=$(cd ${cur_directory} && pwd -P)
-hashcat_deps_dir=${script_directory}/../hashcat-deps
+deps_dir=${script_directory}/../deps
 
-mkdir -p ${hashcat_deps_dir} # but it should already exist (is part of the repository)
-cd ${hashcat_deps_dir}
+mkdir -p ${deps_dir} # but it should already exist (is part of the repository)
+cd ${deps_dir}
 
-## cleanup the directories under the 'hashcat-deps' folder
+## cleanup the directories under the 'deps' folder
 rm -rf {adl-sdk,cuda-7.5,NVIDIA-Linux-x86_64-352.21,nvidia-gdk,amd-app-sdk} && \
 mkdir -p {tmp,adl-sdk,cuda-7.5,NVIDIA-Linux-x86_64-352.21,nvidia-gdk,amd-app-sdk} && \
 cd tmp/
 
 if [ $? -ne 0 ]; then
-  echo "! Cannot create hashcat-deps directories."
+  echo "! Cannot create deps directories."
   exit 1
 fi
 
@@ -34,7 +34,7 @@ for d in ${DOWNLOAD_DEPS}; do
 done
 
 if [ ${i} -gt 0 ]; then
-  echo "! Please manually download all the above dependencies to the hashcat-deps/tmp/ directory"
+  echo "! Please manually download all the above dependencies to the deps/tmp/ directory"
   exit 1
 fi
 
@@ -59,7 +59,7 @@ for pkg in ${DEPS}; do
 done
 
 ## extract ADL SDK
-unzip ADL_SDK8.zip -d ${hashcat_deps_dir}/adl-sdk-8
+unzip ADL_SDK8.zip -d ${deps_dir}/adl-sdk-8
 ret=$?
 
 if [[ ${ret} -ne 0 ]] && [[ ${ret} -ne 1 ]]; then
@@ -67,7 +67,7 @@ if [[ ${ret} -ne 0 ]] && [[ ${ret} -ne 1 ]]; then
   exit 1
 fi
 
-rm -rf ${hashcat_deps_dir}/adl-sdk && ln -s ${hashcat_deps_dir}/adl-sdk-8 ${hashcat_deps_dir}/adl-sdk
+rm -rf ${deps_dir}/adl-sdk && ln -s ${deps_dir}/adl-sdk-8 ${deps_dir}/adl-sdk
 
 if [ $? -ne 0 ]; then
   echo "! failed to setup ADL SDK link"
@@ -75,7 +75,7 @@ if [ $? -ne 0 ]; then
 fi
 
 ## extract NVAPI
-unzip R352-developer.zip -d ${hashcat_deps_dir}
+unzip R352-developer.zip -d ${deps_dir}
 ret=$?
 
 if [[ ${ret} -ne 0 ]] && [[ ${ret} -ne 1 ]]; then
@@ -85,7 +85,7 @@ fi
 
 ## install CUDA SDK
 chmod +x cuda_7.5.18_linux.run && \
-./cuda_7.5.18_linux.run -toolkit -silent -override --toolkitpath=${hashcat_deps_dir}/cuda-7.5
+./cuda_7.5.18_linux.run -toolkit -silent -override --toolkitpath=${deps_dir}/cuda-7.5
 
 if [ $? -ne 0 ]; then
   echo "! failed to install CUDA SDK"
@@ -95,14 +95,14 @@ fi
 ## install NVIDIA Driver
 chmod +x NVIDIA-Linux-x86_64-352.21.run && \
 ./NVIDIA-Linux-x86_64-352.21.run -x && \
-mv NVIDIA-Linux-x86_64-352.21 ${hashcat_deps_dir}/ && \
-cd ${hashcat_deps_dir}/NVIDIA-Linux-x86_64-352.21 && \
+mv NVIDIA-Linux-x86_64-352.21 ${deps_dir}/ && \
+cd ${deps_dir}/NVIDIA-Linux-x86_64-352.21 && \
 ln -s libnvidia-ml.so.352.21 libnvidia-ml.so && \
 ln -s libcuda.so.352.21 libcuda.so && \
 cd 32 && \
 ln -s libnvidia-ml.so.352.21 libnvidia-ml.so && \
 ln -s libcuda.so.352.21 libcuda.so && \
-cd ${hashcat_deps_dir}/tmp
+cd ${deps_dir}/tmp
 
 if [ $? -ne 0 ]; then
   echo "! failed to install NVIDIA Driver"
@@ -111,7 +111,7 @@ fi
 
 ## install NVIDIA GPU Deployment Kit
 chmod +x gdk_linux_amd64_352_55_release.run && \
-./gdk_linux_amd64_352_55_release.run --silent --installdir=${hashcat_deps_dir}/nvidia-gdk
+./gdk_linux_amd64_352_55_release.run --silent --installdir=${deps_dir}/nvidia-gdk
 
 if [ $? -ne 0 ]; then
   echo "! failed to install NVIDIA GPU Deployment Kit"
@@ -120,14 +120,14 @@ fi
 
 ## extract AMD APP SDK
 tar xjf AMD-APP-SDKInstaller-v3.0.130.135-GA-linux64.tar.bz2 && \
-./AMD-APP-SDK-v3.0.130.135-GA-linux64.sh --noexec --target ${hashcat_deps_dir}/amd-app-sdk-v3.0.130.135
+./AMD-APP-SDK-v3.0.130.135-GA-linux64.sh --noexec --target ${deps_dir}/amd-app-sdk-v3.0.130.135
 
 if [ $? -ne 0 ]; then
   echo "! failed to extract AMD APP SDK"
   exit 1
 fi
 
-rm -rf ${hashcat_deps_dir}/amd-app-sdk && ln -s ${hashcat_deps_dir}/amd-app-sdk-v3.0.130.135 ${hashcat_deps_dir}/amd-app-sdk
+rm -rf ${deps_dir}/amd-app-sdk && ln -s ${deps_dir}/amd-app-sdk-v3.0.130.135 ${deps_dir}/amd-app-sdk
 
 if [ $? -ne 0 ]; then
   echo "! failed to setup ADL SDK link"
