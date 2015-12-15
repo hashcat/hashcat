141d5740accd774bdf40bb32e5bb48bd25db5892
jsteube <jens.steube@gmail.com>
2015-12-15 19:28:27 +0100

    update packages for source kernel distribution

--- tools/package_bin.sh
+++ tools/package_bin.sh
@@ -24,24 +24,23 @@ cp -r $IN/rules                                 $OUT/
 cp -r $IN/extra                                 $OUT/
 cp    $IN/example.dict                          $OUT/
 cp    $IN/example[0123456789]*.hash             $OUT/
-cp    $IN/oclExample[0123456789]*.sh            $OUT/
-cp    $IN/oclExample[0123456789]*.cmd           $OUT/
+cp    $IN/example[0123456789]*.sh               $OUT/
+cp    $IN/example[0123456789]*.cmd              $OUT/
 
 cp -r $IN/kernels/4098/*.llvmir                 $OUT/kernels/4098/
+cp -r $IN/include                               $OUT/
+cp -r $IN/OpenCL                                $OUT/
 
 dos2unix $OUT/rules/*.rule
 dos2unix $OUT/rules/hybrid/*.rule
 dos2unix $OUT/docs/*
 dos2unix $OUT/example*
-dos2unix $OUT/*Example*.sh
-dos2unix $OUT/*Example*.cmd
 
 unix2dos $OUT/masks/*.hcmask
 unix2dos $OUT/rules/*.rule
 unix2dos $OUT/rules/hybrid/*.rule
 unix2dos $OUT/docs/*
-unix2dos $OUT/example*
-unix2dos $OUT/*Example*.cmd
+unix2dos $OUT/example*.cmd
 
 chmod 700 $OUT
 chmod 700 $OUT/kernels
@@ -56,13 +55,16 @@ chmod 700 $OUT/charsets/*
 chmod 700 $OUT/masks
 chmod 600 $OUT/masks/*
 chmod 600 $OUT/example*
-chmod 600 $OUT/*Example*.cmd
-chmod 700 $OUT/*Example*.sh
+chmod 700 $OUT/example*.sh
 chmod 700 $OUT/extra
 chmod 700 $OUT/extra/tab_completion/*.sh
 chmod 700 $OUT/extra/tab_completion/install
 chmod 600 $OUT/extra/rules_optimize/*.exe
 chmod 700 $OUT/extra/rules_optimize/*.bin
+chmod 700 $OUT/include
+chmod 600 $OUT/include/*
+chmod 700 $OUT/OpenCL
+chmod 600 $OUT/OpenCL/*
 chmod 600 $OUT/*.exe
 chmod 700 $OUT/*.bin
 chmod 600 $OUT/hashcat.hcstat
