3061b8b2d80f90c787d50449147f4c72bc96f13f
KOLANICH <kolan_n@mail.ru>
2016-08-23 22:10:35 +0300

    replaced swap_workaround macro with definition of byte_swap_32 from bit_ops.h PS I know that code copying is shit

--- include/rp_kernel_on_cpu.h
+++ include/rp_kernel_on_cpu.h
@@ -6,7 +6,21 @@
 #ifndef RP_CPU_H
 #define RP_CPU_H
 
-#define swap_workaround(n) __builtin_bswap32(n)
+#ifdef _MSC_VER
+u32 swap_workaround(const u32 n);
+inline u32 swap_workaround(const u32 n)
+{
+#ifdef _MSC_VER
+  return _byteswap_ulong(n);
+#elif defined(__GNU_C__) || defined(__MINGW32__)
+  return _builtin_bswap32(n);
+#else
+  return (n & 0xff000000) >> 24
+    | (n & 0x00ff0000) >> 8
+    | (n & 0x0000ff00) << 8
+    | (n & 0x000000ff) << 24;
+#endif
+}
 
 #include "common.h"
 #include "inc_rp.h"
