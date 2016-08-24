47f6a9771bebda155c8681203da6da42708a74bf
KOLANICH <kolan_n@mail.ru>
2016-08-24 23:03:56 +0300

    fix error with unneeded #ifdef in rp_kernel_on_cpu.h moved includes into beginning

--- include/rp_kernel_on_cpu.h
+++ include/rp_kernel_on_cpu.h
@@ -7,7 +7,10 @@
 #ifndef RP_CPU_H
 #define RP_CPU_H
 
-#ifdef _MSC_VER
+#include "common.h"
+#include "inc_rp.h"
+
+
 u32 swap_workaround(const u32 n);
 inline u32 swap_workaround(const u32 n)
 {
@@ -22,9 +25,6 @@ inline u32 swap_workaround(const u32 n)
     | (n & 0x000000ff) << 24;
 #endif
 }
-
-#include "common.h"
-#include "inc_rp.h"
 
 u32 apply_rule(const u32 name, const u32 p0, const u32 p1, u32 buf0[4], u32 buf1[4], const u32 in_len);
 u32 apply_rules(u32 *cmds, u32 buf0[4], u32 buf1[4], const u32 len);
