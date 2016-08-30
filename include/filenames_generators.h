693649fadb5ee6898a71d3b9d55e349e80385a16
KOLANICH <kolan_n@mail.ru>
2016-08-30 11:08:21 +0300

    types in filenames_generators.h

--- include/filenames_generators.h
+++ include/filenames_generators.h
@@ -4,16 +4,16 @@
 #include "consts/optimizer_options.h"
 #include "consts/hashcat_modes.h"
 
-void generate_source_kernel_filename(const uint attack_exec, const uint attack_kern, const uint kern_type, char *shared_dir, char *source_file);
+void generate_source_kernel_filename(const ATTACK_EXEC_SIDE_KERNEL attack_exec, const ATTACK_KERN attack_kern, const uint kern_type, const char *shared_dir, char *source_file);
 
-void generate_cached_kernel_filename(const uint attack_exec, const uint attack_kern, const uint kern_type, char *profile_dir, const char *device_name_chksum, char *cached_file);
+void generate_cached_kernel_filename(const ATTACK_EXEC_SIDE_KERNEL attack_exec, const ATTACK_KERN attack_kern, const uint kern_type, char *profile_dir, const char *device_name_chksum, char *cached_file);
 
-void generate_source_kernel_mp_filename(const uint opti_type, const uint opts_type, char *shared_dir, char *source_file);
+void generate_source_kernel_mp_filename(const OPTI_TYPE opti_type, const OPTS_TYPE opts_type, char *shared_dir, char *source_file);
 
-void generate_cached_kernel_mp_filename(const uint opti_type, const uint opts_type, char *profile_dir, const char *device_name_chksum, char *cached_file);
+void generate_cached_kernel_mp_filename(const OPTI_TYPE opti_type, const OPTS_TYPE opts_type, char *profile_dir, const char *device_name_chksum, char *cached_file);
 
-void generate_source_kernel_amp_filename(const uint attack_kern, char *shared_dir, char *source_file);
+void generate_source_kernel_amp_filename(const ATTACK_KERN attack_kern, char *shared_dir, char *source_file);
 
-void generate_cached_kernel_amp_filename(const uint attack_kern, char *profile_dir, const char *device_name_chksum, char *cached_file);
+void generate_cached_kernel_amp_filename(const ATTACK_KERN attack_kern, char *profile_dir, const char *device_name_chksum, char *cached_file);
 
 char *filename_from_filepath(char *filepath);
