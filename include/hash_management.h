963cda3db6bcd3a5404b2ee3915921728cce85f1
jsteube <jens.steube@gmail.com>
2016-09-14 19:50:53 +0200

    Move more functions to hash_management.c

--- include/hash_management.h
+++ include/hash_management.h
@@ -8,6 +8,11 @@
 
 #define USERNAME 0
 
+int sort_by_digest_p0p1  (const void *v1, const void *v2);
+int sort_by_salt         (const void *v1, const void *v2);
+int sort_by_hash         (const void *v1, const void *v2);
+int sort_by_hash_no_salt (const void *v1, const void *v2);
+
 void save_hash ();
 
 void check_hash (hc_device_param_t *device_param, plain_t *plain);
