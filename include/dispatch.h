dcdd0d89cd9734033a994c5b26cd62a4b0de2334
jsteube <jens.steube@gmail.com>
2016-09-15 04:29:22 +0200

    Move thread_monitor() to monitor.c

---
+++ include/dispatch.h
@@ -0,0 +1,12 @@
+/**
+ * Author......: See docs/credits.txt
+ * License.....: MIT
+ */
+
+#ifndef _DISPATCH_H
+#define _DISPATCH_H
+
+void *thread_calc_stdin (void *p);
+void *thread_calc (void *p);
+
+#endif // _DISPATCH_H
---
+++ include/monitor.h
@@ -0,0 +1,11 @@
+/**
+ * Author......: See docs/credits.txt
+ * License.....: MIT
+ */
+
+#ifndef _MONITOR_H
+#define _MONITOR_H
+
+void *thread_monitor (void *p);
+
+#endif // _MONITOR_H
