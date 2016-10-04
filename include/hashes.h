6d3277ab1741fa8e090a28abebde34a8623a5328
jsteube <jens.steube@gmail.com>
2016-10-04 16:18:49 +0200

    Created build_plain(), build_crackpos() and build_debugdata() out of code from check_hash()

--- include/hashes.h
+++ include/hashes.h
@@ -11,6 +11,10 @@ int sort_by_salt         (const void *v1, const void *v2);
 int sort_by_hash         (const void *v1, const void *v2, void *v3);
 int sort_by_hash_no_salt (const void *v1, const void *v2, void *v3);
 
+void build_plain     (hashcat_ctx_t *hashcat_ctx, hc_device_param_t *device_param, plain_t *plain, u32 *plain_buf, int *out_len);
+void build_crackpos  (hashcat_ctx_t *hashcat_ctx, hc_device_param_t *device_param, plain_t *plain, u64 *out_pos);
+void build_debugdata (hashcat_ctx_t *hashcat_ctx, hc_device_param_t *device_param, plain_t *plain, u8 *debug_rule_buf, int *debug_rule_len, u8 *debug_plain_ptr, int *debug_plain_len);
+
 void save_hash (const user_options_t *user_options, const hashconfig_t *hashconfig, const hashes_t *hashes);
 
 void check_hash (hashcat_ctx_t *hashcat_ctx, hc_device_param_t *device_param, plain_t *plain);
