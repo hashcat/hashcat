17d2726c247ca3a655c9c5dfea65444e0ddf3f4d
jsteube <jens.steube@gmail.com>
2016-11-12 15:56:51 +0100

    Comment out some unused code but do not remove it to keep up the logic of the code visible

--- src/interface.c
+++ src/interface.c
@@ -543,40 +543,40 @@ static void md5crypt_encode (u8 digest[16], u8 buf[22])
   buf[ 0] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 1] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 2] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[ 3] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[ 3] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 1] << 16) | (digest[ 7] << 8) | (digest[13] << 0);
 
   buf[ 4] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 5] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 6] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[ 7] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[ 7] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 2] << 16) | (digest[ 8] << 8) | (digest[14] << 0);
 
   buf[ 8] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 9] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[10] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[11] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[11] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 3] << 16) | (digest[ 9] << 8) | (digest[15] << 0);
 
   buf[12] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[13] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[14] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[15] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[15] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 4] << 16) | (digest[10] << 8) | (digest[ 5] << 0);
 
   buf[16] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[17] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[18] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[19] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[19] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[11] << 0);
 
   buf[20] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[21] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[21] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 }
 
 static void sha512crypt_decode (u8 digest[64], u8 buf[86])
@@ -787,152 +787,152 @@ static void sha512crypt_encode (u8 digest[64], u8 buf[86])
   buf[ 0] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 1] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 2] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[ 3] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[ 3] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[22] << 16) | (digest[43] << 8) | (digest[ 1] << 0);
 
   buf[ 4] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 5] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 6] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[ 7] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[ 7] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[44] << 16) | (digest[ 2] << 8) | (digest[23] << 0);
 
   buf[ 8] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 9] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[10] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[11] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[11] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 3] << 16) | (digest[24] << 8) | (digest[45] << 0);
 
   buf[12] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[13] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[14] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[15] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[15] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[25] << 16) | (digest[46] << 8) | (digest[ 4] << 0);
 
   buf[16] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[17] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[18] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[19] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[19] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[47] << 16) | (digest[ 5] << 8) | (digest[26] << 0);
 
   buf[20] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[21] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[22] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[23] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[23] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 6] << 16) | (digest[27] << 8) | (digest[48] << 0);
 
   buf[24] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[25] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[26] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[27] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[27] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[28] << 16) | (digest[49] << 8) | (digest[ 7] << 0);
 
   buf[28] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[29] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[30] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[31] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[31] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[50] << 16) | (digest[ 8] << 8) | (digest[29] << 0);
 
   buf[32] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[33] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[34] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[35] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[35] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 9] << 16) | (digest[30] << 8) | (digest[51] << 0);
 
   buf[36] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[37] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[38] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[39] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[39] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[31] << 16) | (digest[52] << 8) | (digest[10] << 0);
 
   buf[40] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[41] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[42] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[43] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[43] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[53] << 16) | (digest[11] << 8) | (digest[32] << 0);
 
   buf[44] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[45] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[46] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[47] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[47] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[12] << 16) | (digest[33] << 8) | (digest[54] << 0);
 
   buf[48] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[49] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[50] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[51] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[51] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[34] << 16) | (digest[55] << 8) | (digest[13] << 0);
 
   buf[52] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[53] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[54] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[55] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[55] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[56] << 16) | (digest[14] << 8) | (digest[35] << 0);
 
   buf[56] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[57] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[58] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[59] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[59] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[15] << 16) | (digest[36] << 8) | (digest[57] << 0);
 
   buf[60] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[61] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[62] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[63] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[63] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[37] << 16) | (digest[58] << 8) | (digest[16] << 0);
 
   buf[64] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[65] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[66] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[67] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[67] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[59] << 16) | (digest[17] << 8) | (digest[38] << 0);
 
   buf[68] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[69] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[70] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[71] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[71] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[18] << 16) | (digest[39] << 8) | (digest[60] << 0);
 
   buf[72] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[73] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[74] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[75] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[75] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[40] << 16) | (digest[61] << 8) | (digest[19] << 0);
 
   buf[76] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[77] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[78] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[79] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[79] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[62] << 16) | (digest[20] << 8) | (digest[41] << 0);
 
   buf[80] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[81] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[82] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[83] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[83] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l =                  0 |                 0 | (digest[63] << 0);
 
   buf[84] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[85] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[85] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 }
 
 static void sha1aix_decode (u8 digest[20], u8 buf[27])
@@ -1235,7 +1235,7 @@ static void sha256aix_encode (u8 digest[32], u8 buf[43])
 
   buf[40] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[41] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[42] = int_to_itoa64 (l & 0x3f);
+  buf[42] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 }
 
 static void sha512aix_decode (u8 digest[64], u8 buf[86])
@@ -1591,7 +1591,7 @@ static void sha512aix_encode (u8 digest[64], u8 buf[86])
   l =                 0 |                 0 | (digest[63] << 16);
 
   buf[84] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[85] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[85] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 }
 
 static void sha256crypt_decode (u8 digest[32], u8 buf[43])
@@ -1705,76 +1705,76 @@ static void sha256crypt_encode (u8 digest[32], u8 buf[43])
   buf[ 0] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 1] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 2] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[ 3] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[ 3] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[21] << 16) | (digest[ 1] << 8) | (digest[11] << 0);
 
   buf[ 4] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 5] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 6] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[ 7] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[ 7] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[12] << 16) | (digest[22] << 8) | (digest[ 2] << 0);
 
   buf[ 8] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[ 9] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[10] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[11] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[11] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 3] << 16) | (digest[13] << 8) | (digest[23] << 0);
 
   buf[12] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[13] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[14] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[15] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[15] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[24] << 16) | (digest[ 4] << 8) | (digest[14] << 0);
 
   buf[16] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[17] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[18] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[19] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[19] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[15] << 16) | (digest[25] << 8) | (digest[ 5] << 0);
 
   buf[20] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[21] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[22] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[23] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[23] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 6] << 16) | (digest[16] << 8) | (digest[26] << 0);
 
   buf[24] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[25] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[26] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[27] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[27] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[27] << 16) | (digest[ 7] << 8) | (digest[17] << 0);
 
   buf[28] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[29] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[30] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[31] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[31] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[18] << 16) | (digest[28] << 8) | (digest[ 8] << 0);
 
   buf[32] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[33] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[34] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[35] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[35] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l = (digest[ 9] << 16) | (digest[19] << 8) | (digest[29] << 0);
 
   buf[36] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[37] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[38] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[39] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[39] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 
   l =                  0 | (digest[31] << 8) | (digest[30] << 0);
 
   buf[40] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[41] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[42] = int_to_itoa64 (l & 0x3f);
+  buf[42] = int_to_itoa64 (l & 0x3f); //l >>= 6;
 }
 
 static void drupal7_decode (u8 digest[64], u8 buf[44])
@@ -1991,7 +1991,7 @@ static void drupal7_encode (u8 digest[64], u8 buf[43])
 
   buf[40] = int_to_itoa64 (l & 0x3f); l >>= 6;
   buf[41] = int_to_itoa64 (l & 0x3f); l >>= 6;
-  buf[42] = int_to_itoa64 (l & 0x3f); l >>= 6;
+  buf[42] = int_to_itoa64 (l & 0x3f); //l >>= 6;
   //buf[43] = int_to_itoa64 (l & 0x3f);
 }
 
