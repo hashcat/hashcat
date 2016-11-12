45ffc764edcfe202dfa208bf162dcc74ac5788ea
jsteube <jens.steube@gmail.com>
2016-11-12 15:34:17 +0100

    Fixed a bug in rule generator code when using the memory copy function

--- src/rp.c
+++ src/rp.c
@@ -234,7 +234,7 @@ int generate_random_rule (char rule_buf[RP_RULE_BUFSIZ], const u32 rp_gen_func_m
         p1 = get_random_num (0, sizeof (grp_pos));
         rule_buf[rule_pos++] = grp_pos[p1];
         p2 = get_random_num (1, sizeof (grp_pos));
-        rule_buf[rule_pos++] = grp_pos[p1];
+        rule_buf[rule_pos++] = grp_pos[p2];
         p3 = get_random_num (0, sizeof (grp_pos));
         rule_buf[rule_pos++] = grp_pos[p3];
         break;
