fc38f36701567ffc634143d025c942d33cec1dda
jsteube <jens.steube@gmail.com>
2016-11-22 18:00:44 +0100

    Help the user to fix the coolbits problem as warning not error

--- src/hwmon.c
+++ src/hwmon.c
@@ -1359,9 +1359,13 @@ static int hm_XNVCTRL_get_fan_control (hashcat_ctx_t *hashcat_ctx, const int gpu
   if (rc == false)
   {
     event_log_error (hashcat_ctx, "XNVCTRLQueryTargetAttribute() failed");
-    event_log_error (hashcat_ctx, "This error typically occurs when you did not setup NVidia Coolbits.");
-    event_log_error (hashcat_ctx, "Run the following command to fix: sudo nvidia-xconfig --cool-bits=12");
-    event_log_error (hashcat_ctx, "Do not forget to restart X afterwards.");
+
+    // help the user to fix the problem
+
+    event_log_warning (hashcat_ctx, "This error typically occurs when you did not setup NVidia Coolbits.");
+    event_log_warning (hashcat_ctx, "Run the following command to fix: sudo nvidia-xconfig --cool-bits=12");
+    event_log_warning (hashcat_ctx, "Do not forget to restart X afterwards.");
+    event_log_warning (hashcat_ctx, "");
 
     return -1;
   }
