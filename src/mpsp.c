24a3aff5eaa9b18d4abd9005ebfe4f00ce77b219
jsteube <jens.steube@gmail.com>
2016-12-18 16:01:23 +0100

    Fixed mask length check in hybrid attack-modes: Do not include hash-mode dependant mask length checks

--- src/mpsp.c
+++ src/mpsp.c
@@ -936,15 +936,19 @@ static int mask_append (hashcat_ctx_t *hashcat_ctx, const char *mask, const char
   {
     const u32 mask_length = mp_get_length (mask);
 
-    const u32 pw_min = hashconfig->pw_min;
-    const u32 pw_max = hashconfig->pw_max;
-
     u32 increment_min = user_options->increment_min;
     u32 increment_max = user_options->increment_max;
 
-    increment_min = MAX (increment_min, pw_min);
-    increment_max = MIN (increment_max, pw_max);
     increment_max = MIN (increment_max, mask_length);
+
+    if (user_options->attack_mode == ATTACK_MODE_BF)
+    {
+      const u32 pw_min = hashconfig->pw_min;
+      const u32 pw_max = hashconfig->pw_max;
+
+      increment_min = MAX (increment_min, pw_min);
+      increment_max = MIN (increment_max, pw_max);
+    }
 
     for (u32 increment_len = increment_min; increment_len <= increment_max; increment_len++)
     {
