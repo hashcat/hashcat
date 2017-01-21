ec0f074f2dd9563a92dfaa647888790ae44205d5
jsteube <jens.steube@gmail.com>
2017-01-21 18:14:44 +0100

    Fix travis-ci error caused by missing __builtin_bswap16()

--- src/bitops.c
+++ src/bitops.c
@@ -45,7 +45,10 @@ u64 rotr64 (const u64 a, const u64 n)
 
 u16 byte_swap_16 (const u16 n)
 {
-  #if defined (_MSC_VER)
+  #if defined(__APPLE__)
+  return (n & 0xff00) >> 8
+       | (n & 0x00ff) << 8;
+  #elif defined (_MSC_VER)
   return _byteswap_ushort (n);
   #elif defined (__clang__) || defined (__GNUC__)
   return __builtin_bswap16 (n);
