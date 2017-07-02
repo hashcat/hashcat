84bc2edd5bfcb0645bbf82270cf476f18eb6a4f1
jsteube <jens.steube@gmail.com>
2017-07-02 15:18:08 +0200

    Add code generators for switch_buffer_by_offset_16x4_be_S()

---
+++ tools/code_generators/GEN_AMD_switch_buffer_by_offset_16x4_be_S.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+for (my $i = 0, my $s = 0; $i < 16; $i++, $s++)
+{
+  printf ("    case %2d:\n", $i);
+  for (my $j = 16 - 1; $j >= 0; $j--)
+  {
+    my $jd4   = $j / 4;
+    my $jm4   = $j % 4;
+
+    my $js1d4 = ($j - $s - 1) / 4;
+    my $js1m4 = ($j - $s - 1) % 4;
+
+    my $js0d4 = ($j - $s - 0) / 4;
+    my $js0m4 = ($j - $s - 0) % 4;
+
+    if ((($j - $s - 1) >= 0) && (($j - $s - 0) >= 0))
+    {
+      printf ("      w%d[%d] = amd_bytealign_S (w%d[%d], w%d[%d], offset);\n", $jd4, $jm4, $js1d4, $js1m4, $js0d4, $js0m4);
+    }
+    elsif (($j - $s - 0) >= 0)
+    {
+      printf ("      w%d[%d] = amd_bytealign_S (    0, w%d[%d], offset);\n", $jd4, $jm4, $js0d4, $js0m4);
+    }
+    else
+    {
+      printf ("      w%d[%d] = 0;\n", $jd4, $jm4);
+    }
+  }
+  printf ("\n");
+
+  printf ("      break;\n");
+  printf ("\n");
+}
