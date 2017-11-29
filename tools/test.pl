c9d352743c5cbd3488eaee7c6fed6d509185cb61
jsteube <jens.steube@gmail.com>
2017-11-29 13:29:08 +0100

    Unit tests for mode 16100

--- tools/test.pl
+++ tools/test.pl
@@ -50,7 +50,7 @@ my $hashcat = "./hashcat";
 
 my $MAX_LEN = 55;
 
-my @modes = (0, 10, 11, 12, 20, 21, 22, 23, 30, 40, 50, 60, 100, 101, 110, 111, 112, 120, 121, 122, 125, 130, 131, 132, 133, 140, 141, 150, 160, 200, 300, 400, 500, 600, 900, 1000, 1100, 1300, 1400, 1410, 1411, 1420, 1430, 1440, 1441, 1450, 1460, 1500, 1600, 1700, 1710, 1711, 1720, 1730, 1740, 1722, 1731, 1750, 1760, 1800, 2100, 2400, 2410, 2500, 2600, 2611, 2612, 2711, 2811, 3000, 3100, 3200, 3710, 3711, 3300, 3500, 3610, 3720, 3800, 3910, 4010, 4110, 4210, 4300, 4400, 4500, 4520, 4521, 4522, 4600, 4700, 4800, 4900, 5000, 5100, 5300, 5400, 5500, 5600, 5700, 5800, 6000, 6100, 6300, 6400, 6500, 6600, 6700, 6800, 6900, 7000, 7100, 7200, 7300, 7400, 7500, 7700, 7800, 7900, 8000, 8100, 8200, 8300, 8400, 8500, 8600, 8700, 8900, 9100, 9200, 9300, 9400, 9500, 9600, 9700, 9800, 9900, 10000, 10100, 10200, 10300, 10400, 10500, 10600, 10700, 10800, 10900, 11000, 11100, 11200, 11300, 11400, 11500, 11600, 11900, 12000, 12001, 12100, 12200, 12300, 12400, 12600, 12700, 12800, 12900, 13000, 13100, 13200, 13300, 13400, 13500, 13600, 13800, 13900, 14000, 14100, 14400, 14700, 14800, 14900, 15000, 15100, 15200, 15300, 15400, 15500, 15600, 15700, 15900, 16000, 99999);
+my @modes = (0, 10, 11, 12, 20, 21, 22, 23, 30, 40, 50, 60, 100, 101, 110, 111, 112, 120, 121, 122, 125, 130, 131, 132, 133, 140, 141, 150, 160, 200, 300, 400, 500, 600, 900, 1000, 1100, 1300, 1400, 1410, 1411, 1420, 1430, 1440, 1441, 1450, 1460, 1500, 1600, 1700, 1710, 1711, 1720, 1730, 1740, 1722, 1731, 1750, 1760, 1800, 2100, 2400, 2410, 2500, 2600, 2611, 2612, 2711, 2811, 3000, 3100, 3200, 3710, 3711, 3300, 3500, 3610, 3720, 3800, 3910, 4010, 4110, 4210, 4300, 4400, 4500, 4520, 4521, 4522, 4600, 4700, 4800, 4900, 5000, 5100, 5300, 5400, 5500, 5600, 5700, 5800, 6000, 6100, 6300, 6400, 6500, 6600, 6700, 6800, 6900, 7000, 7100, 7200, 7300, 7400, 7500, 7700, 7800, 7900, 8000, 8100, 8200, 8300, 8400, 8500, 8600, 8700, 8900, 9100, 9200, 9300, 9400, 9500, 9600, 9700, 9800, 9900, 10000, 10100, 10200, 10300, 10400, 10500, 10600, 10700, 10800, 10900, 11000, 11100, 11200, 11300, 11400, 11500, 11600, 11900, 12000, 12001, 12100, 12200, 12300, 12400, 12600, 12700, 12800, 12900, 13000, 13100, 13200, 13300, 13400, 13500, 13600, 13800, 13900, 14000, 14100, 14400, 14700, 14800, 14900, 15000, 15100, 15200, 15300, 15400, 15500, 15600, 15700, 15900, 16000, 16100, 99999);
 
 my %is_utf16le      = map { $_ => 1 } qw (30 40 130 131 132 133 140 141 1000 1100 1430 1440 1441 1730 1740 1731 5500 5600 8000 9400 9500 9600 9700 9800 11600 13500 13800);
 my %less_fifteen    = map { $_ => 1 } qw (500 1600 1800 3200 6300 7400 10500 10700);
@@ -2785,6 +2785,38 @@ sub verify
 
       next unless (exists ($db->{$hash_in}) and (! defined ($db->{$hash_in})));
     }
+    # tacacs-plus
+    elsif ($mode == 16100)
+    {
+      ($hash_in, $word) = split ":", $line;
+
+      next unless defined $hash_in;
+      next unless defined $word;
+
+      my @data = split ('\$', $hash_in);
+
+      next unless scalar @data == 6;
+
+      shift @data;
+
+      my $signature = shift @data;
+
+      next unless ($signature eq "tacacs-plus");
+
+      my $auth_version = shift @data;
+
+      next unless ($auth_version eq "0");
+
+      my $session_id      = shift @data;
+      my $encrypted_data  = shift @data;
+      my $sequence        = shift @data;
+
+      $param  = $session_id;
+      $param2 = $encrypted_data;
+      $param3 = $sequence;
+
+      next unless (exists ($db->{$hash_in}) and (! defined ($db->{$hash_in})));
+    }
     else
     {
       print "ERROR: hash mode is not supported\n";
@@ -3195,6 +3227,14 @@ sub verify
     elsif ($mode == 15700)
     {
       $hash_out = gen_hash ($mode, $word, $salt, 0, $param, $param2, $param3, $param4);
+
+      $len = length $hash_out;
+
+      return unless (substr ($line, 0, $len) eq $hash_out);
+    }
+    elsif ($mode == 16100)
+    {
+      $hash_out = gen_hash ($mode, $word, undef, 0, $param, $param2, $param3);
 
       $len = length $hash_out;
 
@@ -3754,6 +3794,10 @@ sub passthrough
       next if length ($word_buf) > 8;
 
       $tmp_hash = gen_hash ($mode, $word_buf, "");
+    }
+    elsif ($mode == 16100)
+    {
+      $tmp_hash = gen_hash ($mode, $word_buf, undef);
     }
     else
     {
@@ -4785,6 +4829,20 @@ sub single
     elsif ($mode == 16000)
     {
       for (my $i = 1; $i < 9; $i++)
+      {
+        if ($len != 0)
+        {
+          rnd ($mode, $len, 0);
+        }
+        else
+        {
+          rnd ($mode, $i, 0);
+        }
+      }
+    }
+    elsif ($mode == 16100)
+    {
+      for (my $i = 1; $i < 32; $i++)
       {
         if ($len != 0)
         {
@@ -8937,6 +8995,75 @@ END_CODE
     $hash_buf = substr ($hash_buf, -10);
 
     $tmp_hash = sprintf ("%s", $hash_buf);
+  }
+  elsif ($mode == 16100)
+  {
+    my $session_id;
+    my $encrypted_data;
+    my $sequence;
+
+    if (defined $additional_param)
+    {
+      $session_id = pack ("H*", $additional_param);
+    }
+    else
+    {
+      $session_id = pack ("H*", randbytes (8));
+    }
+
+    if (defined $additional_param2)
+    {
+      $encrypted_data = pack ("H*", $additional_param2);
+    }
+
+    if (defined $additional_param3)
+    {
+      $sequence = pack ("H*", $additional_param3);
+    }
+    else
+    {
+      $sequence = pack ("H*", "c006");
+    }
+
+    my $key = md5 ($session_id . $word_buf . $sequence);
+
+    if (defined $encrypted_data)
+    {
+      ## verify case
+
+      my $encrypted_data_len = length $encrypted_data;
+
+      my $plain_data = substr ($encrypted_data, 0, 6) ^ substr ($key, 0, 6);
+
+      my ($status, $flags, $server_msg_len, $data_len) = unpack ("CCSS", $plain_data);
+
+      if ((($status >= 0x01 && $status <= 0x07) || $status == 0x21)
+       &&  ($flags  == 0x01 || $flags  == 0x00)
+       &&  (6 + $server_msg_len + $data_len == $encrypted_data_len))
+      {
+        ## ok
+      }
+      else
+      {
+        $plain_data = "\xff\xff\xff\xff\xff\xff"; # some invalid data
+      }
+
+      my $plain_data_filtered = substr ($plain_data, 0, 6) ^ substr ($key, 0, 6);
+
+      $encrypted_data = $plain_data_filtered . substr ($encrypted_data, 6);
+    }
+    else
+    {
+      my $plain_data = "\x01\x00\x00\x00\x00\x00";
+
+      my $plain_data_len = length $plain_data;
+
+      my $shortest = ($plain_data_len > 16) ? 16 : $plain_data_len;
+
+      $encrypted_data = substr ($plain_data, 0, $shortest) ^ substr ($key, 0, $shortest);
+    }
+
+    $tmp_hash = sprintf ('$tacacs-plus$0$%s$%s$%s', unpack ("H*", $session_id), unpack ("H*", $encrypted_data), unpack ("H*", $sequence));
   }
   elsif ($mode == 99999)
   {
