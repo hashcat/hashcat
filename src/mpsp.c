1c25cf549048aa8ce3a055b700a2ad3bce5fb051
jsteube <jens.steube@gmail.com>
2018-02-07 22:32:01 +0100

    Fix compiler warning about rc variable which shadows a previous local

--- src/mpsp.c
+++ src/mpsp.c
@@ -1387,9 +1387,9 @@ int mask_ctx_init (hashcat_ctx_t *hashcat_ctx)
   mask_ctx->root_table_buf   = (hcstat_table_t *) hccalloc (SP_ROOT_CNT,   sizeof (hcstat_table_t));
   mask_ctx->markov_table_buf = (hcstat_table_t *) hccalloc (SP_MARKOV_CNT, sizeof (hcstat_table_t));
 
-  const int rc = sp_setup_tbl (hashcat_ctx);
+  const int rc_setup_tbl = sp_setup_tbl (hashcat_ctx);
 
-  if (rc == -1) return -1;
+  if (rc_setup_tbl == -1) return -1;
 
   mask_ctx->root_css_buf   = (cs_t *) hccalloc (SP_PW_MAX,           sizeof (cs_t));
   mask_ctx->markov_css_buf = (cs_t *) hccalloc (SP_PW_MAX * CHARSIZ, sizeof (cs_t));
