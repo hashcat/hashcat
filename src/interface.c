e79feb0b6f9f0b56913f56e440740e0a619f31ef
Jens Steube <jens.steube@gmail.com>
2018-02-17 22:16:05 +0100

    Add more reqd_work_group_size attributes to kernels

--- src/interface.c
+++ src/interface.c
@@ -25963,9 +25963,16 @@ u32 hashconfig_forced_kernel_threads (hashcat_ctx_t *hashcat_ctx)
   if (hashconfig->hash_mode ==  1500) kernel_threads = 64; // DES
   if (hashconfig->hash_mode ==  3000) kernel_threads = 64; // DES
   if (hashconfig->hash_mode ==  3100) kernel_threads = 64; // DES
+  if (hashconfig->hash_mode ==  8500) kernel_threads = 64; // DES
+  if (hashconfig->hash_mode == 14000) kernel_threads = 64; // DES
+  if (hashconfig->hash_mode == 14100) kernel_threads = 64; // DES
+  if (hashconfig->hash_mode == 16000) kernel_threads = 64; // DES
+
+  // this should have a kernel hint attribute in the kernel files
+  // __attribute__((reqd_work_group_size(X, 1, 1)))
+
   if (hashconfig->hash_mode ==  3200) kernel_threads = 8;  // Blowfish
   if (hashconfig->hash_mode ==  7500) kernel_threads = 64; // RC4
-  if (hashconfig->hash_mode ==  8500) kernel_threads = 64; // DES
   if (hashconfig->hash_mode ==  8900) kernel_threads = 16; // SCRYPT
   if (hashconfig->hash_mode ==  9000) kernel_threads = 8;  // Blowfish
   if (hashconfig->hash_mode ==  9300) kernel_threads = 8;  // SCRYPT
@@ -25977,10 +25984,7 @@ u32 hashconfig_forced_kernel_threads (hashcat_ctx_t *hashcat_ctx)
   if (hashconfig->hash_mode == 10410) kernel_threads = 64; // RC4
   if (hashconfig->hash_mode == 10500) kernel_threads = 64; // RC4
   if (hashconfig->hash_mode == 13100) kernel_threads = 64; // RC4
-  if (hashconfig->hash_mode == 14000) kernel_threads = 64; // DES
-  if (hashconfig->hash_mode == 14100) kernel_threads = 64; // DES
   if (hashconfig->hash_mode == 15700) kernel_threads = 1;  // SCRYPT
-  if (hashconfig->hash_mode == 16000) kernel_threads = 64; // DES
 
   return kernel_threads;
 }
