bb758d66e8345931dbc4a6f22261cab83657cb34
Jens Steube <jens.steube@gmail.com>
2018-11-12 14:34:44 +0100

    Revert "Simplify some code"
    This reverts commit 2d15fa07e7eb853266d5e723ff67fcb3894c66e8.

--- src/opencl.c
+++ src/opencl.c
@@ -2794,10 +2794,9 @@ int run_cracker (hashcat_ctx_t *hashcat_ctx, hc_device_param_t *device_param, co
             total_msec += device_param->speed_msec[speed_pos];
           }
 
-          if ((total_msec > 4000) || (device_param->speed_pos == SPEED_CACHE - 1))
+          if (user_options->slow_candidates == true)
           {
-
-            if (user_options->slow_candidates == true)
+            if ((total_msec > 4000) || (device_param->speed_pos == SPEED_CACHE - 1))
             {
               const u32 speed_pos = device_param->speed_pos;
 
@@ -2806,13 +2805,24 @@ int run_cracker (hashcat_ctx_t *hashcat_ctx, hc_device_param_t *device_param, co
                 device_param->speed_cnt[0]  = device_param->speed_cnt[speed_pos - 1];
                 device_param->speed_msec[0] = device_param->speed_msec[speed_pos - 1];
               }
+
+              device_param->speed_pos = 1;
+
+              device_param->speed_only_finish = true;
+
+              break;
             }
+          }
+          else
+          {
+            // it's unclear if 4s is enough to turn on boost mode for all opencl device
 
-            device_param->speed_pos = 1;
+            if ((total_msec > 4000) || (device_param->speed_pos == SPEED_CACHE - 1))
+            {
+              device_param->speed_only_finish = true;
 
-            device_param->speed_only_finish = true;
-
-            break;
+              break;
+            }
           }
         }
       }
