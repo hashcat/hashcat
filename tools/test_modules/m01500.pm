864bd3451e95aabf5ba36687af00c6b41f8e1867
jsteube <jens.steube@gmail.com>
2018-12-21 14:16:31 +0100

    Add test module for -m 1500

---
+++ tools/test_modules/m01500.pm
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+
+##
+## Author......: See docs/credits.txt
+## License.....: MIT
+##
+
+use strict;
+use warnings;
+
+use Digest::MD4 qw (md4_hex);
+use Encode;
+
+sub module_generate_hash
+{
+  my $word = shift;
+  my $salt = shift // random_numeric_string (2);
+
+  return if length $word > 8;
+
+  my $hash = crypt ($word, $salt);
+
+  return $hash;
+}
+
+sub module_verify_hash
+{
+  my $line = shift;
+
+  my ($hash, $word) = split ":", $line;
+
+  return unless defined $hash;
+  return unless defined $word;
+
+  $word = pack_if_HEX_notation ($word);
+
+  my $salt = substr ($hash, 0, 2);
+
+  my $new_hash = module_generate_hash ($word, $salt);
+
+  return unless defined $new_hash;
+
+  return unless $new_hash eq $hash;
+
+  return $new_hash;
+}
+
+1;
