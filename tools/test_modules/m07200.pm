6a387bf704fb36bdae23b077d7f0d4b8c8e25c7d
jsteube <jens.steube@gmail.com>
2019-02-15 21:10:02 +0100

    Add -m 7200 unit test

--- tools/test_modules/m07200.pm
+++ tools/test_modules/m07200.pm
@@ -20,18 +20,17 @@ sub module_generate_hash
 
   if (length $salt == 0)
   {
-    $salt = random_hex_string (64, 64);
+    $salt = random_hex_string (128, 128);
   }
 
-  my $pbkdf2 = Crypt::PBKDF2->new
-  (
+  my $pbkdf2 = Crypt::PBKDF2->new (
     hasher     => Crypt::PBKDF2->hasher_from_algorithm ('HMACSHA2', 512),
     iterations => $iter
   );
 
   my $hash_buf = unpack ("H*", $pbkdf2->PBKDF2 (pack ("H*", $salt), $word));
 
-  my $hash = sprintf ("\$ml\$%i\$%s\$%0128s", $iter, $salt, $hash_buf);
+  my $hash = sprintf ("grub.pbkdf2.sha512.%i.%s.%0128s", $iter, $salt, $hash_buf);
 
   return $hash;
 }
@@ -48,24 +47,22 @@ sub module_verify_hash
 
   my $word = substr ($line, $index1 + 1);
 
-  my $index2 = index ($hash_in, "\$", 5);
+  my $index2 =  index ($hash_in, ".", 19);
 
   return if $index2 < 1;
 
-  my $index3 = index ($hash_in, "\$", $index2 + 1);
+  my $index3 =  index ($hash_in, ".", $index2 + 1);
 
   my $salt = substr ($hash_in, $index2 + 1, $index3 - $index2 - 1);
 
-  my $iter = substr ($hash_in, 4, $index2 - 4);
-
-  return if (int ($iter) < 1);
+  my $iter = substr ($hash_in, 19, $index2 - 19);
 
   return unless defined $salt;
   return unless defined $word;
 
   $word = pack_if_HEX_notation ($word);
 
-  my $new_hash = module_generate_hash ($word, $salt, $iter);
+  my $new_hash = module_generate_hash ($word, $salt);
 
   return ($new_hash, $word);
 }
