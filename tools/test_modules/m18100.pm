6e8c4f5577d5f95e66946a9648cf664cae972651
jsteube <jens.steube@gmail.com>
2019-02-15 14:33:42 +0100

    Fix minimum salt length in -m 18100 unit test

--- tools/test_modules/m18100.pm
+++ tools/test_modules/m18100.pm
@@ -11,7 +11,7 @@ use warnings;
 use Digest::SHA  qw (sha1);
 use Digest::HMAC qw (hmac_hex);
 
-sub module_constraints { [[0, 255], [0, 55], [0, 255], [0, 55], [-1, -1]] }
+sub module_constraints { [[0, 255], [8, 12], [0, 255], [8, 12], [-1, -1]] }
 
 sub module_generate_hash
 {
