c4602a225548e7e82c7e153a3a5b60c00f577a91
jsteube <jens.steube@gmail.com>
2019-02-17 09:37:50 +0100

    Replace -m 1800 und and -m 7400 unit test code with system code

--- tools/test_modules/m07400.pm
+++ tools/test_modules/m07400.pm
@@ -8,179 +8,24 @@
 use strict;
 use warnings;
 
-use Digest::SHA qw (sha256);
-
 sub module_constraints { [[0, 255], [0, 16], [0, 15], [0, 16], [-1, -1]] }
-
-sub to64
-{
-  my $v = shift;
-  my $n = shift;
-
-  my $itoa64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-  my $ret = "";
-
-  while (($n - 1) >= 0)
-  {
-    $n = $n - 1;
-
-    $ret .= substr ($itoa64, $v & 0x3f, 1);
-
-    $v = $v >> 6;
-  }
-
-  return $ret
-}
-
-sub sha256_crypt
-{
-  my $iter = shift;
-  my $pass = shift;
-  my $salt = shift;
-
-  my $hash = ""; # hash to be returned by this function
-
-  my $final = sha256 ($pass . $salt . $pass);
-
-  $salt = substr ($salt, 0, 16);
-
-  my $tmp = $pass . $salt;
-
-  my $pass_len = length ($pass);
-  my $salt_len = length ($salt);
-
-  my $i;
-
-  for ($i = $pass_len; $i > 0; $i -= 16)
-  {
-    my $len = 16;
-
-    if ($i < $len)
-    {
-      $len = $i;
-    }
-
-    $tmp .= substr ($final, 0, $len);
-  }
-
-  $i = $pass_len;
-
-  while ($i > 0)
-  {
-    if ($i & 1)
-    {
-      $tmp .= $final;
-    }
-    else
-    {
-      $tmp .= $pass;
-    }
-
-    $i >>= 1;
-  }
-
-  $final = sha256 ($tmp);
-
-  # p_bytes
-
-  my $p_bytes = "";
-
-  for ($i = 0; $i < $pass_len; $i++)
-  {
-    $p_bytes .= $pass;
-  }
-
-  $p_bytes = sha256 ($p_bytes);
-  $p_bytes = substr ($p_bytes, 0, $pass_len);
-
-  # s_bytes
-
-  my $final_first_byte = ord (substr ($final, 0, 1));
-
-  my $s_bytes = "";
-
-  for ($i = 0; $i < (16 + $final_first_byte); $i++)
-  {
-    $s_bytes .= $salt;
-  }
-
-  $s_bytes = sha256 ($s_bytes);
-  $s_bytes = substr ($s_bytes, 0, $salt_len);
-
-  for ($i = 0; $i < $iter; $i++)
-  {
-    $tmp = "";
-
-    if ($i & 1)
-    {
-      $tmp .= $p_bytes;
-    }
-    else
-    {
-      $tmp .= $final;
-    }
-
-    if ($i % 3)
-    {
-      $tmp .= $s_bytes;
-    }
-
-    if ($i % 7)
-    {
-      $tmp .= $p_bytes;
-    }
-
-    if ($i & 1)
-    {
-      $tmp .= $final;
-    }
-    else
-    {
-      $tmp .= $p_bytes;
-    }
-
-    $final = sha256 ($tmp);
-  }
-
-  # done
-  # now format the output string ("hash")
-
-  my $hash_buf;
-
-  $hash .= to64 ((ord (substr ($final,  0, 1)) << 16) | (ord (substr ($final, 10, 1)) << 8) | (ord (substr ($final, 20, 1))), 4);
-  $hash .= to64 ((ord (substr ($final, 21, 1)) << 16) | (ord (substr ($final,  1, 1)) << 8) | (ord (substr ($final, 11, 1))), 4);
-  $hash .= to64 ((ord (substr ($final, 12, 1)) << 16) | (ord (substr ($final, 22, 1)) << 8) | (ord (substr ($final,  2, 1))), 4);
-  $hash .= to64 ((ord (substr ($final,  3, 1)) << 16) | (ord (substr ($final, 13, 1)) << 8) | (ord (substr ($final, 23, 1))), 4);
-  $hash .= to64 ((ord (substr ($final, 24, 1)) << 16) | (ord (substr ($final,  4, 1)) << 8) | (ord (substr ($final, 14, 1))), 4);
-  $hash .= to64 ((ord (substr ($final, 15, 1)) << 16) | (ord (substr ($final, 25, 1)) << 8) | (ord (substr ($final,  5, 1))), 4);
-  $hash .= to64 ((ord (substr ($final,  6, 1)) << 16) | (ord (substr ($final, 16, 1)) << 8) | (ord (substr ($final, 26, 1))), 4);
-  $hash .= to64 ((ord (substr ($final, 27, 1)) << 16) | (ord (substr ($final,  7, 1)) << 8) | (ord (substr ($final, 17, 1))), 4);
-  $hash .= to64 ((ord (substr ($final, 18, 1)) << 16) | (ord (substr ($final, 28, 1)) << 8) | (ord (substr ($final,  8, 1))), 4);
-  $hash .= to64 ((ord (substr ($final,  9, 1)) << 16) | (ord (substr ($final, 19, 1)) << 8) | (ord (substr ($final, 29, 1))), 4);
-  $hash .= to64 ((ord (substr ($final, 31, 1)) <<  8) | (ord (substr ($final, 30, 1))), 3);
-
-  my $magic = '$5$';
-
-  if ($iter == 5000) # default
-  {
-    $hash_buf = sprintf ("%s%s\$%s", $magic, $salt , $hash);
-  }
-  else
-  {
-    $hash_buf = sprintf ("%srounds=%i\$%s\$%s", $magic, $iter, $salt , $hash);
-  }
-
-  return $hash_buf;
-}
 
 sub module_generate_hash
 {
   my $word = shift;
   my $salt = shift;
-  my $iter = shift // 5000;
+  my $iter = shift;
 
-  my $hash_buf = sha256_crypt ($iter, $word, $salt);
+  my $hash_buf;
+
+  if (defined $iter)
+  {
+    $hash_buf = crypt ($word, "\$5\$rounds=$iter\$$salt\$");
+  }
+  else
+  {
+    $hash_buf = crypt ($word, "\$5\$$salt\$");
+  }
 
   my $hash = sprintf ("%s", $hash_buf);
 
