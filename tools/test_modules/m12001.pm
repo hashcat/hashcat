b30495b9014b6e9950bd3e1f7f707608ac347d7a
jsteube <jens.steube@gmail.com>
2019-02-19 09:34:58 +0100

    Update constraints for new unit tests

--- tools/test_modules/m12001.pm
+++ tools/test_modules/m12001.pm
@@ -11,13 +11,13 @@ use warnings;
 use MIME::Base64 qw (encode_base64 decode_base64);
 use Crypt::PBKDF2;
 
-sub module_constraints { [[0, 255], [16, 16], [0, 55], [16, 16], [-1, -1]] }
+sub module_constraints { [[0, 255], [16, 16], [-1, -1], [-1, -1], [-1, -1]] }
 
 sub module_generate_hash
 {
   my $word  = shift;
   my $salt  = shift;
- 
+
   my $pbkdf2 = Crypt::PBKDF2->new
   (
     hasher     => Crypt::PBKDF2->hasher_from_algorithm ('HMACSHA1'),
