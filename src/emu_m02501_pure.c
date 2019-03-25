2bb594b38514e657eba24e5f1b65bc831237171d
jsteube <jens.steube@gmail.com>
2019-03-25 16:40:39 +0100

    Remove add3() from bitops, replaced with the ones in inc_common.cl

--- src/emu_m02501_pure.c
+++ src/emu_m02501_pure.c
@@ -12,6 +12,18 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunknown-pragmas"
 
+#define DGST_ELEM 4
+#define DGST_POS0 0
+#define DGST_POS1 1
+#define DGST_POS2 2
+#define DGST_POS3 3
+
+typedef struct digest
+{
+  u32 digest_buf[DGST_ELEM];
+
+} digest_t;
+
 #include "m02501-pure.cl"
 
 #pragma GCC diagnostic pop
