027af75a396be3d0ade6440a3dbb51b2c1ce1158
Jens Steube <jens.steube@gmail.com>
2019-05-08 20:42:46 +0200

    Fix rotate function names

--- OpenCL/inc_platform.h
+++ OpenCL/inc_platform.h
@@ -14,14 +14,14 @@ DECLSPEC size_t get_global_id   (const u32 dimindx __attribute__((unused)));
 DECLSPEC size_t get_local_id    (const u32 dimindx __attribute__((unused)));
 DECLSPEC size_t get_local_size  (const u32 dimindx __attribute__((unused)));
 
-DECLSPEC u32x hc_rotl32   (const u32x a, const int n);
-DECLSPEC u32x hc_rotr32   (const u32x a, const int n);
-DECLSPEC u32  hc_rotl32_S (const u32  a, const int n);
-DECLSPEC u32  hc_rotr32_S (const u32  a, const int n);
-DECLSPEC u64x hc_rotl64   (const u64x a, const int n);
-DECLSPEC u64x hc_rotr64   (const u64x a, const int n);
-DECLSPEC u64  hc_rotl64_S (const u64  a, const int n);
-DECLSPEC u64  hc_rotr64_S (const u64  a, const int n);
+DECLSPEC u32x rotl32   (const u32x a, const int n);
+DECLSPEC u32x rotr32   (const u32x a, const int n);
+DECLSPEC u32  rotl32_S (const u32  a, const int n);
+DECLSPEC u32  rotr32_S (const u32  a, const int n);
+DECLSPEC u64x rotl64   (const u64x a, const int n);
+DECLSPEC u64x rotr64   (const u64x a, const int n);
+DECLSPEC u64  rotl64_S (const u64  a, const int n);
+DECLSPEC u64  rotr64_S (const u64  a, const int n);
 
 //#define rotate(a,n) (((a) << (n)) | ((a) >> (32 - (n))))
 #define bitselect(a,b,c) ((a) ^ ((c) & ((b) ^ (a))))
