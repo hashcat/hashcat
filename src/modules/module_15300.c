9cc20877f0fc72b4004b0a5df72beaa6114fe0fe
Jens Steube <jens.steube@gmail.com>
2019-06-25 13:29:51 +0200

    Mark DPAPI masterkey file cracking on macOS as unstable

--- src/modules/module_15300.c
+++ src/modules/module_15300.c
@@ -98,6 +98,15 @@ u32 module_pw_max (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED con
 
 bool module_unstable_warning (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra, MAYBE_UNUSED const hc_device_param_t *device_param)
 {
+  if (device_param->opencl_platform_vendor_id == VENDOR_ID_APPLE)
+  {
+    // trap 6
+    if ((device_param->opencl_device_vendor_id == VENDOR_ID_INTEL_SDK) && (device_param->opencl_device_type & CL_DEVICE_TYPE_GPU))
+    {
+      return true;
+    }
+  }
+
   // amdgpu-pro-18.50-708488-ubuntu-18.04: self-test failed
   if ((device_param->opencl_device_vendor_id == VENDOR_ID_AMD) && (device_param->has_vperm == false))
   {
