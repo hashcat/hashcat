b171b7f5dbc10e36d1727b52766ecc875d16396c
Rosen Penev <rosenp@gmail.com>
2019-09-09 16:53:01 -0700

    Fix compilation with system zlib

--- include/ext_lzma.h
+++ include/ext_lzma.h
@@ -8,8 +8,13 @@
 #include <LzmaDec.h>
 #include <Lzma2Dec.h>
 
+#if defined(USE_SYSTEM_ZLIB) && USE_SYSTEM_ZLIB
+#include <minizip/ioapi.h>
+#include <minizip/unzip.h>
+#else
 #include "contrib/minizip/ioapi.h"
 #include "contrib/minizip/unzip.h"
+#endif
 
 int hc_lzma1_decompress (const unsigned char *in, SizeT *in_len, unsigned char *out, SizeT *out_len, const char *props);
 int hc_lzma2_decompress (const unsigned char *in, SizeT *in_len, unsigned char *out, SizeT *out_len, const char *props);
