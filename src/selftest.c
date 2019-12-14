e83c71a18ff1664c41f23d79669c017d909ec4ad
Jens Steube <jens.steube@gmail.com>
2019-12-14 18:44:13 +0100

    Add missing AUX4 in selftest.c

--- src/selftest.c
+++ src/selftest.c
@@ -554,6 +554,10 @@ static int selftest (hashcat_ctx_t *hashcat_ctx, hc_device_param_t *device_param
       {
         if (run_kernel (hashcat_ctx, device_param, KERN_RUN_AUX3, 1, false, 0) == -1) return -1;
       }
+      else if (hashconfig->opts_type & OPTS_TYPE_AUX4)
+      {
+        if (run_kernel (hashcat_ctx, device_param, KERN_RUN_AUX4, 1, false, 0) == -1) return -1;
+      }
       else
       {
         if (run_kernel (hashcat_ctx, device_param, KERN_RUN_3, 1, false, 0) == -1) return -1;
