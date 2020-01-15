ae03120981bbac206dfb38696a7ebc4499ba149c
Jens Steube <jens.steube@gmail.com>
2020-01-15 10:33:36 +0100

    Update HASH_NAME in -m 22301

--- src/modules/module_22301.c
+++ src/modules/module_22301.c
@@ -17,7 +17,7 @@ static const u32   DGST_POS2      = 2;
 static const u32   DGST_POS3      = 6;
 static const u32   DGST_SIZE      = DGST_SIZE_4_8;
 static const u32   HASH_CATEGORY  = HASH_CATEGORY_RAW_HASH_SALTED;
-static const char *HASH_NAME      = "Telegram (SHA256)";
+static const char *HASH_NAME      = "Telegram client app passcode (SHA256)";
 static const u64   KERN_TYPE      = 22300;
 static const u32   OPTI_TYPE      = OPTI_TYPE_ZERO_BYTE
                                   | OPTI_TYPE_PRECOMPUTE_INIT
