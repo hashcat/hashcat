5fab483cf0925cb85947f163465d839f6fe0e2d3
Jens Steube <jens.steube@gmail.com>
2020-07-21 12:41:20 +0200

    Compile ZLIB: Fixed makefile include paths in case USE_SYSTEM_ZLIB is used

--- include/ext_lzma.h
+++ include/ext_lzma.h
@@ -8,8 +8,8 @@
 #include <LzmaDec.h>
 #include <Lzma2Dec.h>
 
-#include "contrib/minizip/ioapi.h"
-#include "contrib/minizip/unzip.h"
+#include "minizip/ioapi.h"
+#include "minizip/unzip.h"
 
 int hc_lzma1_decompress (const unsigned char *in, SizeT *in_len, unsigned char *out, SizeT *out_len, const char *props);
 int hc_lzma2_decompress (const unsigned char *in, SizeT *in_len, unsigned char *out, SizeT *out_len, const char *props);
