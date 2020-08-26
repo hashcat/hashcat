eed7863af41c5804f6e454ab95c223baec829fc0
philsmd <philsmd@hashcat.net>
2020-08-26 12:05:57 +0200

    add OPTS_TYPE_SUGGEST_KG to -m 23100 = Apple Keychain

--- src/modules/module_23100.c
+++ src/modules/module_23100.c
@@ -21,7 +21,8 @@ static const char *HASH_NAME      = "Apple Keychain";
 static const u64   KERN_TYPE      = 23100;
 static const u32   OPTI_TYPE      = OPTI_TYPE_ZERO_BYTE
                                   | OPTI_TYPE_SLOW_HASH_SIMD_LOOP;
-static const u64   OPTS_TYPE      = OPTS_TYPE_PT_GENERATE_LE;
+static const u64   OPTS_TYPE      = OPTS_TYPE_PT_GENERATE_LE
+                                  | OPTS_TYPE_SUGGEST_KG;
 static const u32   SALT_TYPE      = SALT_TYPE_EMBEDDED;
 static const char *ST_PASS        = "hashcat";
 static const char *ST_HASH        = "$keychain$*74cd1efd49e54a8fdc8750288801e09fa26a33b1*66001ad4e0498dc7*5a084b7314971b728cb551ac40b2e50b7b5bd8b8496b902efe7af07538863a45394ead8399ec581681f7416003c49cc7";
