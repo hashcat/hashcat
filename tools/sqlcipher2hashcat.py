5774934eb8e493db0cff840021e127b0cc0b235c
TROUNCE <trounceharry@gmail.com>
2020-10-26 23:07:30 +0000

    Add files via upload

--- tools/sqlcipher2hashcat.py
+++ tools/sqlcipher2hashcat.py
@@ -4,4 +4,4 @@ import sys
 database = open(sys.argv[1], "rb").read(272)
 salt = database[:16]
 
-print('sqlcipher:256000:' + b64encode(salt).decode() + ':' + b64encode(database[16:272]).decode())
+print('sqlcipherv4:256000:' + b64encode(salt).decode() + ':' + b64encode(database[16:272]).decode())
