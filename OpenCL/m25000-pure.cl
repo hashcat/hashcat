d4db641f5825a49b6605fcd085db9b40ca19e2d6
Gabriele Gristina <matrix@users.noreply.github.com>
2020-12-22 07:11:13 +0100

    Added hash-mode 25000 - SNMPv3 HMAC-MD5-96/HMAC-SHA1-96

---
+++ OpenCL/m25000-pure.cl
@@ -0,0 +1,337 @@
+/**
+ * Author......: See docs/credits.txt
+ * License.....: MIT
+ */
+
+//#define NEW_SIMD_CODE
+
+#ifdef KERNEL_STATIC
+#include "inc_vendor.h"
+#include "inc_types.h"
+#include "inc_platform.cl"
+#include "inc_common.cl"
+#include "inc_simd.cl"
+#include "inc_hash_md5.cl"
+#include "inc_hash_sha1.cl"
+#endif
+
+#define SNMPV3_SALT_MAX 375
+#define SNMPV3_ENGINEID_MAX 32
+#define SNMPV3_MSG_AUTH_PARAMS_MAX 12
+
+typedef struct hmac_md5sha1_tmp
+{
+  u32  dgst_md5[4];
+  u32  dgst_sha1[5];
+
+  u32  out_md5[3];
+  u32  out_sha1[3];
+
+} hmac_md5sha1_tmp_t;
+
+typedef struct snmpv3_multi
+{
+  u32 salt_buf_md5[SNMPV3_SALT_MAX];
+  u32 salt_buf_sha1[SNMPV3_SALT_MAX];
+  u32 salt_len;
+
+  u8 engineID_buf[SNMPV3_ENGINEID_MAX];
+  u32 engineID_len;
+
+} snmpv3_multi_t;
+
+KERNEL_FQ void m25000_init (KERN_ATTR_TMPS_ESALT (hmac_md5sha1_tmp_t, snmpv3_multi_t))
+{
+  /**
+   * modifier
+   */
+
+  const u64 lid = get_local_id (0);
+  const u64 gid = get_global_id (0);
+
+  if (gid >= gid_max) return;
+
+  /**
+   * base
+   */
+
+  const global u8 *pw_buf = (global u8 *) pws[gid].i;
+
+  const u32 pw_len = pws[gid].pw_len;
+
+  const global u8 *engineID_buf = esalt_bufs[DIGESTS_OFFSET].engineID_buf;
+
+  u32 engineID_len = esalt_bufs[DIGESTS_OFFSET].engineID_len;
+
+  /**
+   * authkey
+   */
+
+  u32 idx = 0;
+
+  u8 tmp_buf[72] = { 0 };
+
+   md5_ctx_t  md5_ctx;
+  sha1_ctx_t sha1_ctx;
+
+   md5_init  (&md5_ctx);
+  sha1_init (&sha1_ctx);
+
+  for (int j = 0; j < 16384; j++)
+  {
+    for (int i = 0; i < 64; i++)
+    {
+      tmp_buf[i] = pw_buf[idx++];
+
+      if (idx >= pw_len) idx = 0;
+    }
+
+     md5_update      (&md5_ctx , (u32 *)tmp_buf, 64);
+    sha1_update_swap (&sha1_ctx, (u32 *)tmp_buf, 64);
+  }
+
+   md5_final (&md5_ctx);
+  sha1_final (&sha1_ctx);
+
+  const u32 h_md5[4] = {
+    hc_swap32_S (md5_ctx.h[0]),
+    hc_swap32_S (md5_ctx.h[1]),
+    hc_swap32_S (md5_ctx.h[2]),
+    hc_swap32_S (md5_ctx.h[3])
+  };
+
+  const u32 h_sha1[5] = {
+    hc_swap32_S (sha1_ctx.h[0]),
+    hc_swap32_S (sha1_ctx.h[1]),
+    hc_swap32_S (sha1_ctx.h[2]),
+    hc_swap32_S (sha1_ctx.h[3]),
+    hc_swap32_S (sha1_ctx.h[4])
+  };
+
+  u8  md5_buf[72] = { 0 };
+  u8 sha1_buf[72] = { 0 };
+
+  sha1_buf[ 0] =  h_sha1[0] & 0xff;
+  sha1_buf[ 1] = (h_sha1[0] >> 8) & 0xff;
+  sha1_buf[ 2] = (h_sha1[0] >> 16) & 0xff;
+  sha1_buf[ 3] = (h_sha1[0] >> 24) & 0xff;
+
+   md5_buf[ 3] =  h_md5[ 0] & 0xff;
+   md5_buf[ 2] = (h_md5[ 0] >> 8) & 0xff;
+   md5_buf[ 1] = (h_md5[ 0] >> 16) & 0xff;
+   md5_buf[ 0] = (h_md5[ 0] >> 24) & 0xff;
+
+  sha1_buf[ 4] =  h_sha1[1] & 0xff;
+  sha1_buf[ 5] = (h_sha1[1] >> 8) & 0xff;
+  sha1_buf[ 6] = (h_sha1[1] >> 16) & 0xff;
+  sha1_buf[ 7] = (h_sha1[1] >> 24) & 0xff;
+
+   md5_buf[ 7] =  h_md5[ 1] & 0xff;
+   md5_buf[ 6] = (h_md5[ 1] >> 8) & 0xff;
+   md5_buf[ 5] = (h_md5[ 1] >> 16) & 0xff;
+   md5_buf[ 4] = (h_md5[ 1] >> 24) & 0xff;
+
+  sha1_buf[ 8] =  h_sha1[2] & 0xff;
+  sha1_buf[ 9] = (h_sha1[2] >> 8) & 0xff;
+  sha1_buf[10] = (h_sha1[2] >> 16) & 0xff;
+  sha1_buf[11] = (h_sha1[2] >> 24) & 0xff;
+
+   md5_buf[11] =  h_md5[ 2] & 0xff;
+   md5_buf[10] = (h_md5[ 2] >> 8) & 0xff;
+   md5_buf[ 9] = (h_md5[ 2] >> 16) & 0xff;
+   md5_buf[ 8] = (h_md5[ 2] >> 24) & 0xff;
+
+  sha1_buf[12] =  h_sha1[3] & 0xff;
+  sha1_buf[13] = (h_sha1[3] >> 8) & 0xff;
+  sha1_buf[14] = (h_sha1[3] >> 16) & 0xff;
+  sha1_buf[15] = (h_sha1[3] >> 24) & 0xff;
+
+   md5_buf[15] =  h_md5[ 3] & 0xff;
+   md5_buf[14] = (h_md5[ 3] >> 8) & 0xff;
+   md5_buf[13] = (h_md5[ 3] >> 16) & 0xff;
+   md5_buf[12] = (h_md5[ 3] >> 24) & 0xff;
+
+  sha1_buf[16] =  h_sha1[4] & 0xff;
+  sha1_buf[17] = (h_sha1[4] >> 8) & 0xff;
+  sha1_buf[18] = (h_sha1[4] >> 16) & 0xff;
+  sha1_buf[19] = (h_sha1[4] >> 24) & 0xff;
+
+  u32 j;
+  u32 i = 20, o = 16;
+
+  for (j = 0; j < engineID_len; j++)
+  {
+    sha1_buf[i++] = engineID_buf[j];
+     md5_buf[o++] = engineID_buf[j];
+  }
+
+  for (j = 0; j < 16; j++)
+  {
+    sha1_buf[i++] = sha1_buf[j];
+     md5_buf[o++] =  md5_buf[j];
+  }
+
+  for (j = 16; j < 20; j++)
+  {
+    sha1_buf[i++] = sha1_buf[j];
+  }
+
+   md5_init (&md5_ctx);
+  sha1_init (&sha1_ctx);
+
+   md5_update      (&md5_ctx,  md5_buf,  o);
+  sha1_update_swap (&sha1_ctx, sha1_buf, i);
+
+   md5_final (& md5_ctx);
+  sha1_final (&sha1_ctx);
+
+  tmps[gid].dgst_sha1[0] = sha1_ctx.h[0];
+  tmps[gid].dgst_sha1[1] = sha1_ctx.h[1];
+  tmps[gid].dgst_sha1[2] = sha1_ctx.h[2];
+  tmps[gid].dgst_sha1[3] = sha1_ctx.h[3];
+  tmps[gid].dgst_sha1[4] = sha1_ctx.h[4];
+
+  tmps[gid].dgst_md5[ 0] = md5_ctx.h[ 0];
+  tmps[gid].dgst_md5[ 1] = md5_ctx.h[ 1];
+  tmps[gid].dgst_md5[ 2] = md5_ctx.h[ 2];
+  tmps[gid].dgst_md5[ 3] = md5_ctx.h[ 3];
+}
+
+KERNEL_FQ void m25000_loop (KERN_ATTR_TMPS_ESALT (hmac_md5sha1_tmp_t, snmpv3_multi_t))
+{
+  /**
+   * modifier
+   */
+
+  const u64 gid = get_global_id (0);
+
+  if (gid >= gid_max) return;
+
+  u32  md5_key[16] = { 0 };
+  u32 sha1_key[16] = { 0 };
+
+   md5_key[0] = tmps[gid].dgst_md5[ 0];
+   md5_key[1] = tmps[gid].dgst_md5[ 1];
+   md5_key[2] = tmps[gid].dgst_md5[ 2];
+   md5_key[3] = tmps[gid].dgst_md5[ 3];
+
+  sha1_key[0] = tmps[gid].dgst_sha1[0];
+  sha1_key[1] = tmps[gid].dgst_sha1[1];
+  sha1_key[2] = tmps[gid].dgst_sha1[2];
+  sha1_key[3] = tmps[gid].dgst_sha1[3];
+  sha1_key[4] = tmps[gid].dgst_sha1[4];
+
+   md5_hmac_ctx_t  md5_ctx;
+  sha1_hmac_ctx_t sha1_ctx;
+
+   md5_hmac_init (& md5_ctx,  md5_key, 16);
+  sha1_hmac_init (&sha1_ctx, sha1_key, 20);
+
+   md5_hmac_update_global (& md5_ctx, esalt_bufs[DIGESTS_OFFSET].salt_buf_md5,  esalt_bufs[DIGESTS_OFFSET].salt_len);
+  sha1_hmac_update_global (&sha1_ctx, esalt_bufs[DIGESTS_OFFSET].salt_buf_sha1, esalt_bufs[DIGESTS_OFFSET].salt_len);
+
+   md5_hmac_final (& md5_ctx);
+  sha1_hmac_final (&sha1_ctx);
+
+  tmps[gid].out_md5[ 0] =  md5_ctx.opad.h[DGST_R0];
+  tmps[gid].out_md5[ 1] =  md5_ctx.opad.h[DGST_R1];
+  tmps[gid].out_md5[ 2] =  md5_ctx.opad.h[DGST_R2];
+
+  tmps[gid].out_sha1[0] = sha1_ctx.opad.h[DGST_R0];
+  tmps[gid].out_sha1[1] = sha1_ctx.opad.h[DGST_R1];
+  tmps[gid].out_sha1[2] = sha1_ctx.opad.h[DGST_R2];
+}
+
+KERNEL_FQ void m25000_comp (KERN_ATTR_TMPS_ESALT (hmac_md5sha1_tmp_t, snmpv3_multi_t))
+{
+  /**
+   * base
+   */
+
+  const u64 gid = get_global_id (0);
+
+  if (gid >= gid_max) return;
+
+  const u64 lid = get_local_id (0);
+
+  const u32 s0_r0 = hc_swap32_S (tmps[gid].out_md5[DGST_R0]);
+  const u32 s0_r1 = hc_swap32_S (tmps[gid].out_md5[DGST_R1]);
+  const u32 s0_r2 = hc_swap32_S (tmps[gid].out_md5[DGST_R2]);
+  const u32 s0_r3 = 0;
+
+  const u32 s1_r0 = tmps[gid].out_sha1[DGST_R0];
+  const u32 s1_r1 = tmps[gid].out_sha1[DGST_R1];
+  const u32 s1_r2 = tmps[gid].out_sha1[DGST_R2];
+  const u32 s1_r3 = 0;
+
+  #define il_pos 0
+
+  #ifdef KERNEL_STATIC
+
+  u32 digest_tp_s0[4];
+
+  digest_tp_s0[0] = s0_r0;
+  digest_tp_s0[1] = s0_r1;
+  digest_tp_s0[2] = s0_r2;
+  digest_tp_s0[3] = s0_r3;
+
+  u32 digest_tp_s1[4];
+
+  digest_tp_s1[0] = s1_r0;
+  digest_tp_s1[1] = s1_r1;
+  digest_tp_s1[2] = s1_r2;
+  digest_tp_s1[3] = s1_r3;
+
+  if (check (digest_tp_s0,
+             bitmaps_buf_s1_a,
+             bitmaps_buf_s1_b,
+             bitmaps_buf_s1_c,
+             bitmaps_buf_s1_d,
+             bitmaps_buf_s2_a,
+             bitmaps_buf_s2_b,
+             bitmaps_buf_s2_c,
+             bitmaps_buf_s2_d,
+             bitmap_mask,
+             bitmap_shift1,
+             bitmap_shift2))
+  {
+    int digest_pos = find_hash (digest_tp_s0, digests_cnt, &digests_buf[DIGESTS_OFFSET]);
+
+    if (digest_pos != -1)
+    {
+      const u32 final_hash_pos = DIGESTS_OFFSET + digest_pos;
+
+      if (atomic_inc (&hashes_shown[final_hash_pos]) == 0)
+      {
+        mark_hash (plains_buf, d_return_buf, SALT_POS, digests_cnt, digest_pos, final_hash_pos, gid, il_pos, 0, 0);
+      }
+    }
+  }
+
+  if (check (digest_tp_s1,
+             bitmaps_buf_s1_a,
+             bitmaps_buf_s1_b,
+             bitmaps_buf_s1_c,
+             bitmaps_buf_s1_d,
+             bitmaps_buf_s2_a,
+             bitmaps_buf_s2_b,
+             bitmaps_buf_s2_c,
+             bitmaps_buf_s2_d,
+             bitmap_mask,
+             bitmap_shift1,
+             bitmap_shift2))
+  {
+    int digest_pos = find_hash (digest_tp_s1, digests_cnt, &digests_buf[DIGESTS_OFFSET]);
+
+    if (digest_pos != -1)
+    {
+      const u32 final_hash_pos = DIGESTS_OFFSET + digest_pos;
+
+      if (atomic_inc (&hashes_shown[final_hash_pos]) == 0)
+      {
+        mark_hash (plains_buf, d_return_buf, SALT_POS, digests_cnt, digest_pos, final_hash_pos, gid, il_pos, 0, 0);
+      }
+    }
+  }
+  #endif
+}
---
+++ src/modules/module_25000.c
@@ -0,0 +1,269 @@
+/**
+ * Author......: See docs/credits.txt
+ * License.....: MIT
+ */
+
+#include "common.h"
+#include "types.h"
+#include "modules.h"
+#include "bitops.h"
+#include "convert.h"
+#include "shared.h"
+
+static const u32   ATTACK_EXEC    = ATTACK_EXEC_OUTSIDE_KERNEL;
+static const u32   DGST_POS0      = 0;
+static const u32   DGST_POS1      = 1;
+static const u32   DGST_POS2      = 2;
+static const u32   DGST_POS3      = 3;
+static const u32   DGST_SIZE      = DGST_SIZE_4_4; // 4_3
+static const u32   HASH_CATEGORY  = HASH_CATEGORY_NETWORK_PROTOCOL;
+static const char *HASH_NAME      = "SNMPv3 HMAC-MD5-96/HMAC-SHA1-96";
+static const u64   KERN_TYPE      = 25000;
+static const u32   OPTI_TYPE      = OPTI_TYPE_ZERO_BYTE;
+static const u64   OPTS_TYPE      = OPTS_TYPE_PT_GENERATE_LE
+                                  | OPTS_TYPE_HASH_COPY;
+static const u32   SALT_TYPE      = SALT_TYPE_EMBEDDED;
+static const char *ST_PASS        = "pippoxxx";
+static const char *ST_HASH        = "$SNMPv3$0$76$30820144020103301102043cdca370020300ffe304010302010304383036040d80001f888059dc486145a2632202010802020aba0406706970706f33040c0000000000000000000000000408f9a7cd5639adc7de0481f12d4e0febddef162199aa61bb97f44b84d975d9cef001d31eed660a193c22362c2ba6d203932822baa6c5d0032cc5cd7a8b7ac7b2fc005820ea72d72ffe59d3696be2bc8d5bdffb2de6fc775ed26cbf2d49a513704867665126775b8ffcaf3c07c19f9ecefb20293af7a6beecb6a5f2e3ba812ed9d71d21679007546f3acc6b72aff2baff2688451e74434dc9e6dab2f1b5e149691ced9fb4283fc8f85e3e7ebbe833353076fbdea7a11bc13a8c5ea62385b519e8bd2ab15f646572f487c8eb471eb0b069c5cc500eb8abc0227746d4ee8a5d9f0d6bfd9ece27f3f99ad5937c3e9be08e3074963796d3a13907fa1f17d213$80001f888059dc486145a26322$3de2a23a91ef278f8277b3f5";
+
+u32         module_attack_exec    (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return ATTACK_EXEC;     }
+u32         module_dgst_pos0      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_POS0;       }
+u32         module_dgst_pos1      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_POS1;       }
+u32         module_dgst_pos2      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_POS2;       }
+u32         module_dgst_pos3      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_POS3;       }
+u32         module_dgst_size      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_SIZE;       }
+u32         module_hash_category  (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return HASH_CATEGORY;   }
+const char *module_hash_name      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return HASH_NAME;       }
+u64         module_kern_type      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return KERN_TYPE;       }
+u32         module_opti_type      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return OPTI_TYPE;       }
+u64         module_opts_type      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return OPTS_TYPE;       }
+u32         module_salt_type      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return SALT_TYPE;       }
+const char *module_st_hash        (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return ST_HASH;         }
+const char *module_st_pass        (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return ST_PASS;         }
+
+static const char *SIGNATURE_SNMPV3 = "$SNMPv3$0$";
+
+#define SNMPV3_SALT_MAX 1500
+#define SNMPV3_SALT_MAX_BIN 375
+#define SNMPV3_ENGINEID_MAX 32
+#define SNMPV3_MSG_AUTH_PARAMS_MAX 12
+
+typedef struct hmac_md5sha1_tmp
+{
+  u32  dgst_md5[4];
+  u32  dgst_sha1[5];
+
+  u32  out_md5[3];
+  u32  out_sha1[3];
+
+} hmac_md5sha1_tmp_t;
+
+typedef struct snmpv3_multi
+{
+  u32 salt_buf_md5[SNMPV3_SALT_MAX_BIN];
+  u32 salt_buf_sha1[SNMPV3_SALT_MAX_BIN];
+  u32 salt_len;
+
+  u8 engineID_buf[SNMPV3_ENGINEID_MAX];
+  u32 engineID_len;
+
+} snmpv3_multi_t;
+
+u64 module_esalt_size (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra)
+{
+  const u64 esalt_size = (const u64) sizeof (snmpv3_multi_t);
+
+  return esalt_size;
+}
+
+u64 module_tmp_size (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra)
+{
+  const u64 tmp_size = (const u64) sizeof (hmac_md5sha1_tmp_t);
+
+  return tmp_size;
+}
+
+int module_hash_decode (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED void *digest_buf, MAYBE_UNUSED salt_t *salt, MAYBE_UNUSED void *esalt_buf, MAYBE_UNUSED void *hook_salt_buf, MAYBE_UNUSED hashinfo_t *hash_info, const char *line_buf, MAYBE_UNUSED const int line_len)
+{
+  u32 *digest = (u32 *) digest_buf;
+
+  snmpv3_multi_t *snmpv3 = (snmpv3_multi_t *) esalt_buf;
+
+  token_t token;
+
+  token.token_cnt  = 5;
+  token.signatures_cnt    = 1;
+  token.signatures_buf[0] = SIGNATURE_SNMPV3;
+
+  token.len[0]     = 10;
+  token.attr[0]    = TOKEN_ATTR_FIXED_LENGTH
+                   | TOKEN_ATTR_VERIFY_SIGNATURE;
+
+  // packet number
+  token.len_min[1] = 1;
+  token.len_max[1] = 8;
+  token.sep[1]     = '$';
+  token.attr[1]    = TOKEN_ATTR_VERIFY_LENGTH;
+
+  // salt
+  token.len_min[2] = 1 * 2;
+  token.len_max[2] = SNMPV3_SALT_MAX * 2;
+  token.sep[2]     = '$';
+  token.attr[2]    = TOKEN_ATTR_VERIFY_LENGTH
+                   | TOKEN_ATTR_VERIFY_HEX;
+
+  // engineid
+  token.len_min[3] = 5 * 2;
+  token.len_max[3] = SNMPV3_ENGINEID_MAX * 2;
+  token.sep[3]     = '$';
+  token.attr[3]    = TOKEN_ATTR_VERIFY_LENGTH
+                   | TOKEN_ATTR_VERIFY_HEX;
+
+  // digest
+  token.len_min[4] = SNMPV3_MSG_AUTH_PARAMS_MAX * 2;
+  token.len_max[4] = SNMPV3_MSG_AUTH_PARAMS_MAX * 2;
+  token.sep[4]     = '$';
+  token.attr[4]    = TOKEN_ATTR_VERIFY_LENGTH
+                   | TOKEN_ATTR_VERIFY_HEX;
+
+  const int rc_tokenizer = input_tokenizer ((const u8 *) line_buf, line_len, &token);
+
+  if (rc_tokenizer != PARSER_OK) return (rc_tokenizer);
+
+  // salt
+
+  const u8 *salt_pos = token.buf[2];
+  const int salt_len = token.len[2];
+
+  u8 *salt_ptr = (u8 *) snmpv3->salt_buf_md5;
+
+  int i;
+  int j;
+
+  for (i = 0, j = 0; i < salt_len; i += 2, j += 1)
+  {
+    salt_ptr[j] = hex_to_u8 (salt_pos + i);
+  }
+
+  for (i = 0; i < salt_len / 4; i++)
+  {
+    snmpv3->salt_buf_sha1[i] = byte_swap_32 (snmpv3->salt_buf_md5[i]);
+  }
+
+  snmpv3->salt_len = salt_len / 2;
+
+  salt->salt_iter = 1;
+
+  // engineid
+
+  const u8 *engineID_pos = token.buf[3];
+  const int engineID_len = token.len[3];
+
+  u8 *engineID_ptr = (u8 *) snmpv3->engineID_buf;
+
+  for (i = 0, j = 0; i < engineID_len; i += 2, j += 1)
+  {
+    engineID_ptr[j] = hex_to_u8 (engineID_pos + i);
+  }
+
+  snmpv3->engineID_len = engineID_len / 2;
+
+  // digest
+
+  const u8 *hash_pos = token.buf[4];
+
+  digest[0] = hex_to_u32 (hash_pos +  0);
+  digest[1] = hex_to_u32 (hash_pos +  8);
+  digest[2] = hex_to_u32 (hash_pos + 16);
+
+  digest[0] = byte_swap_32 (digest[0]);
+  digest[1] = byte_swap_32 (digest[1]);
+  digest[2] = byte_swap_32 (digest[2]);
+
+  digest[3] = 0;
+
+  return (PARSER_OK);
+}
+
+int module_hash_encode (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const void *digest_buf, MAYBE_UNUSED const salt_t *salt, MAYBE_UNUSED const void *esalt_buf, MAYBE_UNUSED const void *hook_salt_buf, MAYBE_UNUSED const hashinfo_t *hash_info, char *line_buf, MAYBE_UNUSED const int line_size)
+{
+  const int line_len = snprintf (line_buf, line_size, "%s", hash_info->orighash);
+
+  return line_len;
+}
+
+void module_init (module_ctx_t *module_ctx)
+{
+  module_ctx->module_context_size             = MODULE_CONTEXT_SIZE_CURRENT;
+  module_ctx->module_interface_version        = MODULE_INTERFACE_VERSION_CURRENT;
+
+  module_ctx->module_attack_exec              = module_attack_exec;
+  module_ctx->module_benchmark_esalt          = MODULE_DEFAULT;
+  module_ctx->module_benchmark_hook_salt      = MODULE_DEFAULT;
+  module_ctx->module_benchmark_mask           = MODULE_DEFAULT;
+  module_ctx->module_benchmark_salt           = MODULE_DEFAULT;
+  module_ctx->module_build_plain_postprocess  = MODULE_DEFAULT;
+  module_ctx->module_deep_comp_kernel         = MODULE_DEFAULT;
+  module_ctx->module_dgst_pos0                = module_dgst_pos0;
+  module_ctx->module_dgst_pos1                = module_dgst_pos1;
+  module_ctx->module_dgst_pos2                = module_dgst_pos2;
+  module_ctx->module_dgst_pos3                = module_dgst_pos3;
+  module_ctx->module_dgst_size                = module_dgst_size;
+  module_ctx->module_dictstat_disable         = MODULE_DEFAULT;
+  module_ctx->module_esalt_size               = module_esalt_size;
+  module_ctx->module_extra_buffer_size        = MODULE_DEFAULT;
+  module_ctx->module_extra_tmp_size           = MODULE_DEFAULT;
+  module_ctx->module_forced_outfile_format    = MODULE_DEFAULT;
+  module_ctx->module_hash_binary_count        = MODULE_DEFAULT;
+  module_ctx->module_hash_binary_parse        = MODULE_DEFAULT;
+  module_ctx->module_hash_binary_save         = MODULE_DEFAULT;
+  module_ctx->module_hash_decode_potfile      = MODULE_DEFAULT;
+  module_ctx->module_hash_decode_zero_hash    = MODULE_DEFAULT;
+  module_ctx->module_hash_decode              = module_hash_decode;
+  module_ctx->module_hash_encode_status       = MODULE_DEFAULT;
+  module_ctx->module_hash_encode_potfile      = MODULE_DEFAULT;
+  module_ctx->module_hash_encode              = module_hash_encode;
+  module_ctx->module_hash_init_selftest       = MODULE_DEFAULT;
+  module_ctx->module_hash_mode                = MODULE_DEFAULT;
+  module_ctx->module_hash_category            = module_hash_category;
+  module_ctx->module_hash_name                = module_hash_name;
+  module_ctx->module_hashes_count_min         = MODULE_DEFAULT;
+  module_ctx->module_hashes_count_max         = MODULE_DEFAULT;
+  module_ctx->module_hlfmt_disable            = MODULE_DEFAULT;
+  module_ctx->module_hook_extra_param_size    = MODULE_DEFAULT;
+  module_ctx->module_hook_extra_param_init    = MODULE_DEFAULT;
+  module_ctx->module_hook_extra_param_term    = MODULE_DEFAULT;
+  module_ctx->module_hook12                   = MODULE_DEFAULT;
+  module_ctx->module_hook23                   = MODULE_DEFAULT;
+  module_ctx->module_hook_salt_size           = MODULE_DEFAULT;
+  module_ctx->module_hook_size                = MODULE_DEFAULT;
+  module_ctx->module_jit_build_options        = MODULE_DEFAULT;
+  module_ctx->module_jit_cache_disable        = MODULE_DEFAULT;
+  module_ctx->module_kernel_accel_max         = MODULE_DEFAULT;
+  module_ctx->module_kernel_accel_min         = MODULE_DEFAULT;
+  module_ctx->module_kernel_loops_max         = MODULE_DEFAULT;
+  module_ctx->module_kernel_loops_min         = MODULE_DEFAULT;
+  module_ctx->module_kernel_threads_max       = MODULE_DEFAULT;
+  module_ctx->module_kernel_threads_min       = MODULE_DEFAULT;
+  module_ctx->module_kern_type                = module_kern_type;
+  module_ctx->module_kern_type_dynamic        = MODULE_DEFAULT;
+  module_ctx->module_opti_type                = module_opti_type;
+  module_ctx->module_opts_type                = module_opts_type;
+  module_ctx->module_outfile_check_disable    = MODULE_DEFAULT;
+  module_ctx->module_outfile_check_nocomp     = MODULE_DEFAULT;
+  module_ctx->module_potfile_custom_check     = MODULE_DEFAULT;
+  module_ctx->module_potfile_disable          = MODULE_DEFAULT;
+  module_ctx->module_potfile_keep_all_hashes  = MODULE_DEFAULT;
+  module_ctx->module_pwdump_column            = MODULE_DEFAULT;
+  module_ctx->module_pw_max                   = MODULE_DEFAULT;
+  module_ctx->module_pw_min                   = MODULE_DEFAULT;
+  module_ctx->module_salt_max                 = MODULE_DEFAULT;
+  module_ctx->module_salt_min                 = MODULE_DEFAULT;
+  module_ctx->module_salt_type                = module_salt_type;
+  module_ctx->module_separator                = MODULE_DEFAULT;
+  module_ctx->module_st_hash                  = module_st_hash;
+  module_ctx->module_st_pass                  = module_st_pass;
+  module_ctx->module_tmp_size                 = module_tmp_size;
+  module_ctx->module_unstable_warning         = MODULE_DEFAULT;
+  module_ctx->module_warmup_disable           = MODULE_DEFAULT;
+}
