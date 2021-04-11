e591b0039b676809b31dd4e711883d318c49e8e8
Jens Steube <jens.steube@gmail.com>
2021-04-11 14:05:55 +0200

    Fixed invalid handling of outfile folder entries for -m 22000

--- src/outfile_check.c
+++ src/outfile_check.c
@@ -224,22 +224,7 @@ static int outfile_remove (hashcat_ctx_t *hashcat_ctx)
             memset (hash_buf.hook_salt, 0, hashconfig->hook_salt_size);
           }
 
-          int parser_status = PARSER_HASH_LENGTH;
-
-          if (module_ctx->module_hash_decode_potfile != MODULE_DEFAULT)
-          {
-            void *tmps = hcmalloc (hashconfig->tmp_size);
-
-            parser_status = module_ctx->module_hash_decode_potfile (hashconfig, hash_buf.digest, hash_buf.salt, hash_buf.esalt, hash_buf.hook_salt, hash_buf.hash_info, line_buf, line_hash_len, tmps);
-
-            hcfree (tmps);
-          }
-          else
-          {
-            // "normal" case: hash in the outfile is the same as the hash in the original hash file
-
-            parser_status = module_ctx->module_hash_decode (hashconfig, hash_buf.digest, hash_buf.salt, hash_buf.esalt, hash_buf.hook_salt, hash_buf.hash_info, line_buf, line_hash_len);
-          }
+          int parser_status = module_ctx->module_hash_decode (hashconfig, hash_buf.digest, hash_buf.salt, hash_buf.esalt, hash_buf.hook_salt, hash_buf.hash_info, line_buf, line_hash_len);
 
           if (parser_status != PARSER_OK) continue;
 
