0ba1558e17ffd147614015a7108be8ab870e35a4
Jens Steube <jens.steube@gmail.com>
2021-05-16 09:51:17 +0200

    Dependencies: Updated xxHash from 0.1.0 to v0.8.0 - Stable XXH3

--- deps/xxHash/xxh3.h
+++ deps/xxHash/xxh3.h
@@ -1,6 +1,7 @@
 /*
  * xxHash - Extremely Fast Hash algorithm
- * Copyright (C) 2012-2020 Yann Collet
+ * Development source file for `xxh3`
+ * Copyright (C) 2019-2020 Yann Collet
  *
  * BSD 2-Clause License (https://www.opensource.org/licenses/bsd-license.php)
  *
@@ -32,12 +33,23 @@
  *   - xxHash source repository: https://github.com/Cyan4973/xxHash
  */
 
-
 /*
- * xxhash.c instantiates functions defined in xxhash.h
+ * Note: This file used to host the source code of XXH3_* variants.
+ * during the development period.
+ * The source code is now properly integrated within xxhash.h.
+ *
+ * xxh3.h is no longer useful,
+ * but it is still provided for compatibility with source code
+ * which used to include it directly.
+ *
+ * Programs are now highly discourage to include xxh3.h.
+ * Include `xxhash.h` instead, which is the officially supported interface.
+ *
+ * In the future, xxh3.h will start to generate warnings, then errors,
+ * then it will be removed from source package and from include directory.
  */
 
-#define XXH_STATIC_LINKING_ONLY   /* access advanced declarations */
-#define XXH_IMPLEMENTATION   /* access definitions */
+/* Simulate the same impact as including the old xxh3.h source file */
 
+#define XXH_INLINE_ALL
 #include "xxhash.h"
