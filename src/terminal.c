ea5ddee6e293384070c322b050dd53c6fa31f16e
Jens Steube <jens.steube@gmail.com>
2021-08-05 17:18:10 +0200

    Add autotune event to inform the user about autotune is starting/stopping on startup Fix uninitialized value in terminal.c

--- src/terminal.c
+++ src/terminal.c
@@ -117,7 +117,7 @@ void goodbye_screen (hashcat_ctx_t *hashcat_ctx, const time_t proc_start, const 
   if (user_options->identify    == true) return;
 
   char start_buf[32]; memset (start_buf, 0, sizeof (start_buf));
-  char stop_buf[32];  memset (start_buf, 0, sizeof (stop_buf));
+  char stop_buf[32];  memset (stop_buf,  0, sizeof (stop_buf));
 
   event_log_info_nn (hashcat_ctx, "Started: %s", ctime_r (&proc_start, start_buf));
   event_log_info_nn (hashcat_ctx, "Stopped: %s", ctime_r (&proc_stop,  stop_buf));
