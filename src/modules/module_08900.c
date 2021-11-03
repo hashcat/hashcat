b768f96228f60dbb3158f4b29e0eb62b8527461a
Jens Steube <jens.steube@gmail.com>
2021-11-03 09:08:17 +0100

    Device Threads: The default maximum device thread number has been reduced from 1024 to 256, users can still overwrite with the -T option

--- src/modules/module_08900.c
+++ src/modules/module_08900.c
@@ -420,7 +420,7 @@ const char *module_extra_tuningdb_block (MAYBE_UNUSED const hashconfig_t *hashco
     "GeForce_RTX_3070                                *       8900    1      46       A\n"
     "GeForce_RTX_3090                                *       8900    1      82       A\n"
     "ALIAS_AMD_RX480                                 *       8900    1      15       A\n"
-    "ALIAS_AMD_Vega64                                *       8900    1      31       A\n"
+    "ALIAS_AMD_Vega64                                *       8900    1      28       A\n"
     "ALIAS_AMD_MI100                                 *       8900    1      79       A\n"
     "ALIAS_AMD_RX6900XT                              *       8900    1      59       A\n"
   ;
