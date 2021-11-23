490ef6be74ff526e08882ceb150f0f5143013a64
Jens Steube <jens.steube@gmail.com>
2021-11-23 18:01:14 +0100

    Remove OPTS_TYPE_MP_MULTI_DISABLE from -m 3200 for testing

--- src/modules/module_03200.c
+++ src/modules/module_03200.c
@@ -21,7 +21,6 @@ static const char *HASH_NAME      = "bcrypt $2*$, Blowfish (Unix)";
 static const u64   KERN_TYPE      = 3200;
 static const u32   OPTI_TYPE      = OPTI_TYPE_ZERO_BYTE;
 static const u64   OPTS_TYPE      = OPTS_TYPE_PT_GENERATE_LE
-                                  | OPTS_TYPE_MP_MULTI_DISABLE
                                   | OPTS_TYPE_DYNAMIC_SHARED;
 static const u32   SALT_TYPE      = SALT_TYPE_EMBEDDED;
 static const char *ST_PASS        = "hashcat";
