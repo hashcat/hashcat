760eba95fbf0cb77c98882209a74820220733265
Pikachu <pika@chu>
2022-10-23 18:17:42 +0300

    Updated hash-mode 3730 unit-test to work with two salts

--- tools/test_modules/m03730.pm
+++ tools/test_modules/m03730.pm
@@ -10,14 +10,14 @@ use warnings;
 
 use Digest::MD5 qw (md5_hex);
 
-sub module_constraints { [[0, 256], [0, 256], [0, 55], [0, 23], [0, 55]] }
+sub module_constraints { [[0, 64], [0, 128], [0, 55], [0, 128], [0, 55]] }
 
 sub module_generate_hash
 {
   my $word = shift;
   my $salt1 = shift;
-  my $salt2 = shift || random_numeric_string (128);
-  
+  my $salt2 = shift || random_numeric_string (32);
+
   my $digest = md5_hex ($salt1 . uc(md5_hex ($salt2 . $word)));
 
   my $hash = sprintf ("%s:%s:%s", $digest, $salt1, $salt2);
@@ -29,16 +29,15 @@ sub module_verify_hash
 {
   my $line = shift;
 
-  my ($hash, $salt1, $salt2, $word) = split (':', $line);
+  my ($hash, $salt, $word) = split (':', $line);
 
   return unless defined $hash;
-  return unless defined $salt1;
-  return unless defined $salt2;
+  return unless defined $salt;
   return unless defined $word;
 
-  $word = pack_if_HEX_notation ($word);
+  my $word_packed = pack_if_HEX_notation ($word);
 
-  my $new_hash = module_generate_hash ($word, $salt1, $salt2);
+  my $new_hash = module_generate_hash ($word_packed, $salt);
 
   return ($new_hash, $word);
 }
