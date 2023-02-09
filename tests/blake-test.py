a016e621a702aa94b5f8ac1ca6f1ad2cd66eddb9
Louka <louka@arch-fac.localdomain>
2023-02-09 15:22:51 +0100

    update tests

--- tests/blake-test.py
+++ tests/blake-test.py
@@ -5,8 +5,8 @@ import hashlib
 # Python script to create dataset of passwords/hashes for testing
 
 PASSWD_FILE       = "500-worst-passwords.txt"
-# VALID_HASHES_FILE = "blake2s-valid-hashes.txt"
-VALID_HASHES_FILE = "blake2b-valid-hashes.txt"
+VALID_HASHES_FILE = "blake2s-valid-hashes.txt"
+# VALID_HASHES_FILE = "blake2b-valid-hashes.txt"
 BLAKE2S_HEADER    = "$BLAKE2$"  
 
 # Password source file : https://github.com/danielmiessler/SecLists/blob/master/Passwords/500-worst-passwords.txt
@@ -18,11 +18,12 @@ def createBlake2sDataset(inputFile, outputFile):
     # for each password in file
     for line in passwd.readlines():
         # compute Blake2s hash
-        # d = hashlib.blake2s()
-        d = hashlib.blake2b()
-        d.update(line.encode())
+        d = hashlib.blake2s()
+        # d = hashlib.blake2b()
+        d.update(line.replace('\n', '').encode())
         # encode in base64 and write it
         formattedHash = BLAKE2S_HEADER + d.hexdigest()
+        # print(formattedHash + '\n', end='')
         hashes.write(formattedHash + '\n')
     print("Done")
 
