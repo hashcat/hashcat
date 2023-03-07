e5542839fcac3485cb85671f08b7055230a7a794
PenguinKeeper7 <64717060+PenguinKeeper7@users.noreply.github.com>
2023-03-07 22:19:04 +0000

    Update straight.c

--- src/straight.c
+++ src/straight.c
@@ -289,6 +289,7 @@ int straight_ctx_init (hashcat_ctx_t *hashcat_ctx)
   {
     if (user_options->rp_files_cnt)
     {
+      event_log_info_nn(hashcat_ctx, "Loading Rules");
       if (kernel_rules_load (hashcat_ctx, &straight_ctx->kernel_rules_buf, &straight_ctx->kernel_rules_cnt) == -1) return -1;
     }
     else if (user_options->rp_gen)
