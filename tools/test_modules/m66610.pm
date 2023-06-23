08847b2221f80d43759191a627de75b8b5709361
Dakota <dakota@dasigns.net>
2023-06-22 21:37:09 -0400

    Add DJB2 truncated

---
+++ tools/test_modules/m66610.pm
@@ -0,0 +1,57 @@
+#!/usr/bin/env perl
+
+##
+## Author......: See docs/credits.txt
+## License.....: MIT
+##
+
+use strict;
+use warnings;
+
+sub module_constraints { [[0, 255], [-1, -1], [0, 55], [-1, -1], [-1, -1]] }
+
+sub djb2trunc
+{
+  my $word = shift;
+
+  my @chars = unpack ("C*", $word);
+
+  my $hash = 0;
+
+  while (my $c = shift @chars)
+  {
+    $hash = ($hash * 33) & 0xffffffff;
+    $hash = ($hash + $c) & 0xffffffff;
+  }
+
+  return $hash & 0x0fffffff;
+}
+
+sub module_generate_hash
+{
+  my $word = shift;
+
+  my $digest = djb2trunc ($word);
+
+  my $hash = sprintf ("%08x", $digest);
+
+  return $hash;
+}
+
+sub module_verify_hash
+{
+  my $line = shift;
+
+  my ($hash, $word) = split (':', $line);
+
+  return unless defined $hash;
+  return unless defined $word;
+
+  my $word_packed = pack_if_HEX_notation ($word);
+
+  my $new_hash = module_generate_hash ($word_packed);
+
+  return ($new_hash, $word);
+}
+
+1;
