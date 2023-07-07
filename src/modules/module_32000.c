7b4c2012b75cf6112cf6c5abb739c3c5ba4638de
Hans <6390369+hans-vh@users.noreply.github.com>
2023-07-07 14:56:39 +0200

    Move SHA1 code from kernel to inc_hash_sha1.cl

--- src/modules/module_32000.c
+++ src/modules/module_32000.c
@@ -95,11 +95,23 @@ const char *module_st_pass (MAYBE_UNUSED const hashconfig_t * hashconfig, MAYBE_
   return ST_PASS;
 }
 
+char *module_jit_build_options (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra, MAYBE_UNUSED const hashes_t *hashes, MAYBE_UNUSED const hc_device_param_t *device_param)
+{
+  char *jit_build_options = NULL;
+
+  // Use SHA1 with endianness bug
+  hc_asprintf (&jit_build_options, "-D SHA1_TRANSFORM_SWAP");
+
+  return jit_build_options;
+}
+
 int module_hash_decode (MAYBE_UNUSED const hashconfig_t * hashconfig, MAYBE_UNUSED void *digest_buf, MAYBE_UNUSED salt_t * salt, MAYBE_UNUSED void *esalt_buf, MAYBE_UNUSED void *hook_salt_buf, MAYBE_UNUSED hashinfo_t * hash_info, const char *line_buf, MAYBE_UNUSED const int line_len)
 {
   u32 *digest = (u32 *) digest_buf;
 
   hc_token_t token;
+
+  memset (&token, 0, sizeof (hc_token_t));
 
   token.token_cnt = 3;
 
@@ -222,7 +234,7 @@ void module_init (module_ctx_t * module_ctx)
   module_ctx->module_hook23 = MODULE_DEFAULT;
   module_ctx->module_hook_salt_size = MODULE_DEFAULT;
   module_ctx->module_hook_size = MODULE_DEFAULT;
-  module_ctx->module_jit_build_options = MODULE_DEFAULT;
+  module_ctx->module_jit_build_options = module_jit_build_options;
   module_ctx->module_jit_cache_disable = MODULE_DEFAULT;
   module_ctx->module_kernel_accel_max = MODULE_DEFAULT;
   module_ctx->module_kernel_accel_min = MODULE_DEFAULT;
