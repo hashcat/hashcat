20294ef88390dd16a8096f94a5c6a295995ef3e9
jsteube <jens.steube@gmail.com>
2023-07-20 20:03:58 +0000

    Move test scripts for 17040 to tools/gpg_tests, similar to what we do with TC/VC. Call script from main folder: tools/gpg_tests/example17040.sh

--- tools/gpg_tests/example17040.sh
+++ tools/gpg_tests/example17040.sh
@@ -12,5 +12,5 @@ else
     fi
 fi
 
-$FILE -a0 -m17040 tools/refdata/m17040/ref1-cast5.hash tools/refdata/m17040/ref1-cast5.txt --potfile-disable
-$FILE -a0 -m17040 tools/refdata/m17040/ref2-cast5.hash tools/refdata/m17040/ref2-cast5.txt --potfile-disable
+$FILE -a0 -m17040 tools/gpg_tests/refdata/m17040/ref1-cast5.hash tools/gpg_tests/refdata/m17040/ref1-cast5.txt --potfile-disable
+$FILE -a0 -m17040 tools/gpg_tests/refdata/m17040/ref2-cast5.hash tools/gpg_tests/refdata/m17040/ref2-cast5.txt --potfile-disable
