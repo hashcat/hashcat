bf6a4e34bd70aafea63c2efd309252a6cdf26898
PenguinKeeper7 <jones-adam@live.co.uk>
2024-05-20 10:49:24 +0100

    Fix max iteration values
    https://github.com/hashcat/hashcat/pull/3980#discussion_r1606514918

--- src/modules/module_23400.c
+++ src/modules/module_23400.c
@@ -167,14 +167,14 @@ int module_hash_decode (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSE
   const u32 iter1 = hc_strtoul ((const char *) iter1_pos, NULL, 10);
 
   if (iter1 <     1) return (PARSER_SALT_ITERATION);
-  if (iter1 > 10^10) return (PARSER_SALT_ITERATION);
+  if (iter1 > 4294967294) return (PARSER_SALT_ITERATION);
 
   salt->salt_iter = iter1 - 1;
 
   const u32 iter2 = hc_strtoul ((const char *) iter2_pos, NULL, 10);
 
   if (iter2 <     1) return (PARSER_SALT_ITERATION);
-  if (iter2 > 10^10) return (PARSER_SALT_ITERATION);
+  if (iter2 > 4294967294) return (PARSER_SALT_ITERATION);
 
   salt->salt_iter2 = iter2 - 1;
 
