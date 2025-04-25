7f2df87cc1238f5f564e568f8f402858623d1321
luke <92046606+dunghm19@users.noreply.github.com>
2025-04-25 11:28:04 +0700

    rename 32710 to 33100

--- src/modules/module_33100.c
+++ src/modules/module_33100.c
@@ -18,7 +18,7 @@ static const u32   DGST_POS3 = 1;
 static const u32   DGST_SIZE = DGST_SIZE_4_4;
 static const u32   HASH_CATEGORY = HASH_CATEGORY_RAW_HASH_SALTED;
 static const char* HASH_NAME = "md5($salt.md5($pass).$salt)";
-static const u64   KERN_TYPE = 32710;
+static const u64   KERN_TYPE = 33100;
 static const u32   OPTI_TYPE = OPTI_TYPE_ZERO_BYTE
                               | OPTI_TYPE_PRECOMPUTE_INIT
                               | OPTI_TYPE_EARLY_SKIP;
