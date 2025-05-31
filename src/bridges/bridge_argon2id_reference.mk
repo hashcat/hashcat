c56199f06b51c5a5f187f62147b5700994e747fd
Jens Steube <jens.steube@gmail.com>
2025-05-31 08:59:18 +0200

    Fix bridge_argon2id_reference.mk to compile on MSYS

--- src/bridges/bridge_argon2id_reference.mk
+++ src/bridges/bridge_argon2id_reference.mk
@@ -1,14 +1,30 @@
 
 ARGON2_REFERENCE := deps/phc-winner-argon2-20190702
+ARGON2_REFERENCE_CFLAGS := -I$(ARGON2_REFERENCE)/_hashcat/
 
 ifeq ($(MAKECMDGOALS),binaries)
-ARGON2_REFERENCE_CFLAGS := -I$(ARGON2_REFERENCE)/_hashcat/ -mavx2
+ARGON2_REFERENCE_CFLAGS += -mavx2
 else
-ARGON2_REFERENCE_CFLAGS := -I$(ARGON2_REFERENCE)/_hashcat/ -march=native
+ifeq ($(UNAME),Darwin)
+ifeq ($(IS_APPLE_SILICON),0)
+ARGON2_REFERENCE_CFLAGS += -mavx2
+endif
+else
+ARGON2_REFERENCE_CFLAGS += -march=native
+endif
 endif
 
+ifeq ($(MAKECMDGOALS),binaries)
 bridges/bridge_argon2id_reference.so:  src/bridges/bridge_argon2id_reference.c obj/combined.LINUX.a
-	$(CC_LINUX)  $(CCFLAGS) $(CFLAGS_CROSS_LINUX) $^ -o $@ $(LFLAGS_CROSS_LINUX) -shared -fPIC -D BRIDGE_INTERFACE_VERSION_CURRENT=$(BRIDGE_INTERFACE_VERSION) $(ARGON2_REFERENCE_CFLAGS)
-
+	$(CC_LINUX) $(CCFLAGS) $(CFLAGS_CROSS_LINUX)  $^ -o $@ $(LFLAGS_CROSS_LINUX) -shared -fPIC -D BRIDGE_INTERFACE_VERSION_CURRENT=$(BRIDGE_INTERFACE_VERSION) $(ARGON2_REFERENCE_CFLAGS)
 bridges/bridge_argon2id_reference.dll: src/bridges/bridge_argon2id_reference.c obj/combined.WIN.a
-	$(CC_WIN)    $(CCFLAGS) $(CFLAGS_CROSS_WIN)   $^ -o $@ $(LFLAGS_CROSS_WIN)   -shared -fPIC -D BRIDGE_INTERFACE_VERSION_CURRENT=$(BRIDGE_INTERFACE_VERSION) $(ARGON2_REFERENCE_CFLAGS)
+	$(CC_WIN)   $(CCFLAGS) $(CFLAGS_CROSS_WIN)    $^ -o $@ $(LFLAGS_CROSS_WIN)   -shared -fPIC -D BRIDGE_INTERFACE_VERSION_CURRENT=$(BRIDGE_INTERFACE_VERSION) $(ARGON2_REFERENCE_CFLAGS)
+else
+ifeq ($(SHARED),1)
+bridges/bridge_argon2id_reference.$(BRIDGE_SUFFIX): src/bridges/bridge_argon2id_reference.c $(HASHCAT_LIBRARY)
+	$(CC)       $(CCFLAGS) $(CFLAGS_NATIVE)       $^ -o $@ $(LFLAGS_NATIVE)      -shared -fPIC -D BRIDGE_INTERFACE_VERSION_CURRENT=$(BRIDGE_INTERFACE_VERSION) $(ARGON2_REFERENCE_CFLAGS)
+else
+bridges/bridge_argon2id_reference.$(BRIDGE_SUFFIX): src/bridges/bridge_argon2id_reference.c obj/combined.NATIVE.a
+	$(CC)       $(CCFLAGS) $(CFLAGS_NATIVE)       $^ -o $@ $(LFLAGS_NATIVE)      -shared -fPIC -D BRIDGE_INTERFACE_VERSION_CURRENT=$(BRIDGE_INTERFACE_VERSION) $(ARGON2_REFERENCE_CFLAGS)
+endif
+endif
