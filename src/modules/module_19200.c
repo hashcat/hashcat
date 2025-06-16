619b4a399845015c760553bbb8d9d16c9a413f09
Gabriele Gristina <matrix@users.noreply.github.com>
2025-06-16 07:52:22 +0200

    Modules: Updated module_unstable_warning for hash-mode 19200

--- src/modules/module_19200.c
+++ src/modules/module_19200.c
@@ -58,6 +58,12 @@ bool module_unstable_warning (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE
 {
   if ((device_param->opencl_platform_vendor_id == VENDOR_ID_INTEL_SDK) && (device_param->opencl_device_type & CL_DEVICE_TYPE_CPU))
   {
+    if (strncmp (device_param->device_name, "AMD EPYC", 8) == 0)
+    {
+      // works on Linux: AMD EPYC 7642 48-Core Processor, OpenCL 2.1 (Build 0)
+      return false;
+    }
+
     return true;
   }
 
