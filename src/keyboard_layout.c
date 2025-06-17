hashcat/hashcat

commit 8eb2558a7e589c496dc39eec530b5806709c5aee

Add -m 6211 module and moved some code around

src/keyboard_layout.c
@@ -1,0 +1,210 @@
+/**
+ * Author......: See docs/credits.txt
+ * License.....: MIT
+ */
+
+#include "common.h"
+#include "types.h"
+#include "memory.h"
+#include "filehandling.h"
+#include "shared.h"
+#include "keyboard_layout.h"
+
+static int sort_by_src_len (const void *p1, const void *p2)
+{
+  const keyboard_layout_mapping_t *k1 = (const keyboard_layout_mapping_t *) p1;
+  const keyboard_layout_mapping_t *k2 = (const keyboard_layout_mapping_t *) p2;
+
+  return k1->src_len < k2->src_len;
+}
+
+bool initialize_keyboard_layout_mapping (const char *filename, keyboard_layout_mapping_t *keyboard_layout_mapping, int *keyboard_layout_mapping_cnt)
+{
+  char *line_buf = (char *) hcmalloc (HCBUFSIZ_LARGE);
+
+  FILE *fp = fopen (filename, "r");
+
+  if (fp == NULL) return false;
+
+  int maps_cnt = 0;
+
+  while (!feof (fp))
+  {
+    const size_t line_len = fgetl (fp, line_buf);
+
+    if (line_len == 0) continue;
+
+    token_t token;
+
+    token.token_cnt  = 2;
+
+    token.len_min[0] = 1;
+    token.len_max[0] = 4;
+    token.sep[0]     = 0x09;
+    token.attr[0]    = TOKEN_ATTR_VERIFY_LENGTH;
+
+    token.len_min[1] = 0;
+    token.len_max[1] = 4;
+    token.sep[1]     = 0x09;
+    token.attr[1]    = TOKEN_ATTR_VERIFY_LENGTH;
+
+    const int rc_tokenizer = input_tokenizer ((const u8 *) line_buf, (const int) line_len, &token);
+
+    if (rc_tokenizer != PARSER_OK)
+    {
+      fclose (fp);
+
+      free (line_buf);
+
+      return false;
+    }
+
+    memcpy (&keyboard_layout_mapping[maps_cnt].src_char, token.buf[0], token.len[0]);
+    memcpy (&keyboard_layout_mapping[maps_cnt].dst_char, token.buf[1], token.len[1]);
+
+    keyboard_layout_mapping[maps_cnt].src_len = token.len[0];
+    keyboard_layout_mapping[maps_cnt].dst_len = token.len[1];
+
+    if (maps_cnt == 256)
+    {
+      fclose (fp);
+
+      free (line_buf);
+
+      return false;
+    }
+
+    maps_cnt++;
+  }
+
+  *keyboard_layout_mapping_cnt = maps_cnt;
+
+  fclose (fp);
+
+  free (line_buf);
+
+  // we need to sort this by length to ensure the largest blocks come first in mapping
+
+  qsort (keyboard_layout_mapping, maps_cnt, sizeof (keyboard_layout_mapping_t), sort_by_src_len);
+
+  return true;
+}
+
+int find_keyboard_layout_map (const u32 search, const int search_len, keyboard_layout_mapping_t *s_keyboard_layout_mapping, const int keyboard_layout_mapping_cnt)
+{
+  for (int idx = 0; idx < keyboard_layout_mapping_cnt; idx++)
+  {
+    const u32 src_char = s_keyboard_layout_mapping[idx].src_char;
+    const int src_len  = s_keyboard_layout_mapping[idx].src_len;
+
+    if (src_len == search_len)
+    {
+      const u32 mask = 0xffffffff >> ((4 - search_len) * 8);
+
+      if ((src_char & mask) == (search & mask)) return idx;
+    }
+  }
+
+  return -1;
+}
+
+int execute_keyboard_layout_mapping (u32 plain_buf[64], const int plain_len, keyboard_layout_mapping_t *s_keyboard_layout_mapping, const int keyboard_layout_mapping_cnt)
+{
+  u32 out_buf[16] = { 0 };
+
+  u8 *out_ptr = (u8 *) out_buf;
+
+  int out_len = 0;
+
+  u8 *plain_ptr = (u8 *) plain_buf;
+
+  int plain_pos = 0;
+
+  while (plain_pos < plain_len)
+  {
+    u32 src0 = 0;
+    u32 src1 = 0;
+    u32 src2 = 0;
+    u32 src3 = 0;
+
+    const int rem = MIN (plain_len - plain_pos, 4);
+
+    if (rem > 0) src0 = plain_ptr[plain_pos + 0];
+    if (rem > 1) src1 = plain_ptr[plain_pos + 1];
+    if (rem > 2) src2 = plain_ptr[plain_pos + 2];
+    if (rem > 3) src3 = plain_ptr[plain_pos + 3];
+
+    const u32 src = (src0 <<  0)
+                  | (src1 <<  8)
+                  | (src2 << 16)
+                  | (src3 << 24);
+
+    int src_len;
+
+    for (src_len = rem; src_len > 0; src_len--)
+    {
+      const int idx = find_keyboard_layout_map (src, src_len, s_keyboard_layout_mapping, keyboard_layout_mapping_cnt);
+
+      if (idx == -1) continue;
+
+      u32 dst_char = s_keyboard_layout_mapping[idx].dst_char;
+      int dst_len  = s_keyboard_layout_mapping[idx].dst_len;
+
+      switch (dst_len)
+      {
+        case 1:
+          out_ptr[out_len++] = (dst_char >>  0) & 0xff;
+          break;
+        case 2:
+          out_ptr[out_len++] = (dst_char >>  0) & 0xff;
+          out_ptr[out_len++] = (dst_char >>  8) & 0xff;
+          break;
+        case 3:
+          out_ptr[out_len++] = (dst_char >>  0) & 0xff;
+          out_ptr[out_len++] = (dst_char >>  8) & 0xff;
+          out_ptr[out_len++] = (dst_char >> 16) & 0xff;
+          break;
+        case 4:
+          out_ptr[out_len++] = (dst_char >>  0) & 0xff;
+          out_ptr[out_len++] = (dst_char >>  8) & 0xff;
+          out_ptr[out_len++] = (dst_char >> 16) & 0xff;
+          out_ptr[out_len++] = (dst_char >> 24) & 0xff;
+          break;
+      }
+
+      plain_pos += src_len;
+
+      break;
+    }
+
+    // not matched, keep original
+
+    if (src_len == 0)
+    {
+      out_ptr[out_len] = plain_ptr[plain_pos];
+
+      out_len++;
+
+      plain_pos++;
+    }
+  }
+
+  plain_buf[ 0] = out_buf[ 0];
+  plain_buf[ 1] = out_buf[ 1];
+  plain_buf[ 2] = out_buf[ 2];
+  plain_buf[ 3] = out_buf[ 3];
+  plain_buf[ 4] = out_buf[ 4];
+  plain_buf[ 5] = out_buf[ 5];
+  plain_buf[ 6] = out_buf[ 6];
+  plain_buf[ 7] = out_buf[ 7];
+  plain_buf[ 8] = out_buf[ 8];
+  plain_buf[ 9] = out_buf[ 9];
+  plain_buf[10] = out_buf[10];
+  plain_buf[11] = out_buf[11];
+  plain_buf[12] = out_buf[12];
+  plain_buf[13] = out_buf[13];
+  plain_buf[14] = out_buf[14];
+  plain_buf[15] = out_buf[15];
+
+  return out_len;
+}

include/interface.h
@@ -27,10 +27,6 @@ int  module_filename (const folder_config_t *folder_config, const int hash_mode,
 bool module_load     (hashcat_ctx_t *hashcat_ctx, module_ctx_t *module_ctx, const u32 hash_mode);
 void module_unload   (module_ctx_t *module_ctx);
 
-bool initialize_keyboard_layout_mapping (hashcat_ctx_t *hashcat_ctx, const char *filename, keyboard_layout_mapping_t *keyboard_layout_mapping, int *keyboard_layout_mapping_cnt);
-int  find_keyboard_layout_map (const u32 search, const int search_len, keyboard_layout_mapping_t *s_keyboard_layout_mapping, const int keyboard_layout_mapping_cnt);
-int  execute_keyboard_layout_mapping (u32 plain_buf[64], const int plain_len, keyboard_layout_mapping_t *s_keyboard_layout_mapping, const int keyboard_layout_mapping_cnt);
-
 int         hashconfig_init                 (hashcat_ctx_t *hashcat_ctx);
 void        hashconfig_destroy              (hashcat_ctx_t *hashcat_ctx);
 

include/interface_migrate.h
@@ -788,7 +788,6 @@ typedef enum kern_type
   KERN_TYPE_ANDROIDPIN              = 5800,
   KERN_TYPE_RIPEMD160               = 6000,
   KERN_TYPE_WHIRLPOOL               = 6100,
-  KERN_TYPE_TCRIPEMD160_XTS512      = 6211,
   KERN_TYPE_TCRIPEMD160_XTS1024     = 6212,
   KERN_TYPE_TCRIPEMD160_XTS1536     = 6213,
   KERN_TYPE_TCSHA512_XTS512         = 6221,

include/keyboard_layout.h
@@ -1,0 +1,13 @@
+/**
+ * Author......: See docs/credits.txt
+ * License.....: MIT
+ */
+
+#ifndef _KEYBOARD_LAYOUT_H
+#define _KEYBOARD_LAYOUT_H
+
+bool initialize_keyboard_layout_mapping (const char *filename, keyboard_layout_mapping_t *keyboard_layout_mapping, int *keyboard_layout_mapping_cnt);
+int  find_keyboard_layout_map (const u32 search, const int search_len, keyboard_layout_mapping_t *s_keyboard_layout_mapping, const int keyboard_layout_mapping_cnt);
+int  execute_keyboard_layout_mapping (u32 plain_buf[64], const int plain_len, keyboard_layout_mapping_t *s_keyboard_layout_mapping, const int keyboard_layout_mapping_cnt);
+
+#endif // _KEYBOARD_LAYOUT_H

src/Makefile
@@ -346,7 +346,7 @@ LFLAGS_CROSS_WIN64      += -lws2_32
 ## Objects
 ##
 
-OBJS_ALL                := affinity autotune benchmark bitmap bitops combinator common convert cpt cpu_aes cpu_crc32 cpu_des cpu_md4 cpu_md5 cpu_sha1 cpu_sha256 debugfile dictstat dispatch dynloader event ext_ADL ext_nvapi ext_nvml ext_OpenCL ext_sysfs ext_lzma filehandling folder hashcat hashes hlfmt hwmon induct interface locking logfile loopback memory monitor mpsp opencl outfile_check outfile pidfile potfile restore rp rp_cpu rp_kernel_on_cpu rp_kernel_on_cpu_optimized selftest slow_candidates shared status stdout straight terminal thread timer tuningdb usage user_options wordlist
+OBJS_ALL                := affinity autotune benchmark bitmap bitops combinator common convert cpt cpu_aes cpu_crc32 cpu_des cpu_md4 cpu_md5 cpu_sha1 cpu_sha256 debugfile dictstat dispatch dynloader event ext_ADL ext_nvapi ext_nvml ext_OpenCL ext_sysfs ext_lzma filehandling folder hashcat hashes hlfmt hwmon induct interface keyboard_layout locking logfile loopback memory monitor mpsp opencl outfile_check outfile pidfile potfile restore rp rp_cpu rp_kernel_on_cpu rp_kernel_on_cpu_optimized selftest slow_candidates shared status stdout straight terminal thread timer tuningdb usage user_options wordlist
 
 ifeq ($(ENABLE_BRAIN),1)
 OBJS_ALL                += brain
@@ -389,7 +389,7 @@ WIN_64_OBJS             += $(foreach OBJ,$(OBJS_XXHASH),obj/$(OBJ).WIN.64.o)
 endif
 endif
 
-MODULE_OBJS_ALL                 := bitops convert cpu_aes cpu_crc32 cpu_des cpu_md5 ext_lzma memory shared
+MODULE_OBJS_ALL                 := bitops convert cpu_aes cpu_crc32 cpu_des cpu_md5 ext_lzma filehandling keyboard_layout memory shared
 
 ifeq ($(USE_SYSTEM_LZMA),0)
 MODULE_OBJS_ALL                 += Alloc Lzma2Dec LzmaDec

src/hashes.c
@@ -1497,11 +1497,6 @@ int hashes_init_stage4 (hashcat_ctx_t *hashcat_ctx)
   hashes_t       *hashes       = hashcat_ctx->hashes;
   user_options_t *user_options = hashcat_ctx->user_options;
 
-  // this is a good phase for setting keyfile and keyboard mapping
-  //const int rc_defaults = hashconfig_general_defaults (hashcat_ctx);
-
-  //if (rc_defaults == -1) return -1;
-
   if (hashes->salts_cnt == 1)
     hashconfig->opti_type |= OPTI_TYPE_SINGLE_SALT;
 

src/interface.c
@@ -17,218 +17,6 @@
 #include "dynloader.h"
 
 /**
- * parser
- */
-
-static int sort_by_src_len (const void *p1, const void *p2)
-{
-  const keyboard_layout_mapping_t *k1 = (const keyboard_layout_mapping_t *) p1;
-  const keyboard_layout_mapping_t *k2 = (const keyboard_layout_mapping_t *) p2;
-
-  return k1->src_len < k2->src_len;
-}
-
-bool initialize_keyboard_layout_mapping (hashcat_ctx_t *hashcat_ctx, const char *filename, keyboard_layout_mapping_t *keyboard_layout_mapping, int *keyboard_layout_mapping_cnt)
-{
-  char *line_buf = (char *) hcmalloc (HCBUFSIZ_LARGE);
-
-  FILE *fp = fopen (filename, "r");
-
-  if (fp == NULL)
-  {
-    event_log_error (hashcat_ctx, "%s: %s", filename, strerror (errno));
-
-    return false;
-  }
-
-  int maps_cnt = 0;
-
-  while (!feof (fp))
-  {
-    const size_t line_len = fgetl (fp, line_buf);
-
-    if (line_len == 0) continue;
-
-    token_t token;
-
-    token.token_cnt  = 2;
-
-    token.len_min[0] = 1;
-    token.len_max[0] = 4;
-    token.sep[0]     = 0x09;
-    token.attr[0]    = TOKEN_ATTR_VERIFY_LENGTH;
-
-    token.len_min[1] = 0;
-    token.len_max[1] = 4;
-    token.sep[1]     = 0x09;
-    token.attr[1]    = TOKEN_ATTR_VERIFY_LENGTH;
-
-    const int rc_tokenizer = input_tokenizer ((const u8 *) line_buf, (const int) line_len, &token);
-
-    if (rc_tokenizer != PARSER_OK)
-    {
-      event_log_error (hashcat_ctx, "%s: Syntax error: %s", filename, line_buf);
-
-      fclose (fp);
-
-      free (line_buf);
-
-      return false;
-    }
-
-    memcpy (&keyboard_layout_mapping[maps_cnt].src_char, token.buf[0], token.len[0]);
-    memcpy (&keyboard_layout_mapping[maps_cnt].dst_char, token.buf[1], token.len[1]);
-
-    keyboard_layout_mapping[maps_cnt].src_len = token.len[0];
-    keyboard_layout_mapping[maps_cnt].dst_len = token.len[1];
-
-    if (maps_cnt == 256)
-    {
-      event_log_error (hashcat_ctx, "%s: too many entries", filename);
-
-      fclose (fp);
-
-      free (line_buf);
-
-      return false;
-    }
-
-    maps_cnt++;
-  }
-
-  *keyboard_layout_mapping_cnt = maps_cnt;
-
-  fclose (fp);
-
-  free (line_buf);
-
-  // we need to sort this by length to ensure the largest blocks come first in mapping
-
-  qsort (keyboard_layout_mapping, maps_cnt, sizeof (keyboard_layout_mapping_t), sort_by_src_len);
-
-  return true;
-}
-
-int find_keyboard_layout_map (const u32 search, const int search_len, keyboard_layout_mapping_t *s_keyboard_layout_mapping, const int keyboard_layout_mapping_cnt)
-{
-  for (int idx = 0; idx < keyboard_layout_mapping_cnt; idx++)
-  {
-    const u32 src_char = s_keyboard_layout_mapping[idx].src_char;
-    const int src_len  = s_keyboard_layout_mapping[idx].src_len;
-
-    if (src_len == search_len)
-    {
-      const u32 mask = 0xffffffff >> ((4 - search_len) * 8);
-
-      if ((src_char & mask) == (search & mask)) return idx;
-    }
-  }
-
-  return -1;
-}
-
-int execute_keyboard_layout_mapping (u32 plain_buf[64], const int plain_len, keyboard_layout_mapping_t *s_keyboard_layout_mapping, const int keyboard_layout_mapping_cnt)
-{
-  u32 out_buf[16] = { 0 };
-
-  u8 *out_ptr = (u8 *) out_buf;
-
-  int out_len = 0;
-
-  u8 *plain_ptr = (u8 *) plain_buf;
-
-  int plain_pos = 0;
-
-  while (plain_pos < plain_len)
-  {
-    u32 src0 = 0;
-    u32 src1 = 0;
-    u32 src2 = 0;
-    u32 src3 = 0;
-
-    const int rem = MIN (plain_len - plain_pos, 4);
-
-    if (rem > 0) src0 = plain_ptr[plain_pos + 0];
-    if (rem > 1) src1 = plain_ptr[plain_pos + 1];
-    if (rem > 2) src2 = plain_ptr[plain_pos + 2];
-    if (rem > 3) src3 = plain_ptr[plain_pos + 3];
-
-    const u32 src = (src0 <<  0)
-                  | (src1 <<  8)
-                  | (src2 << 16)
-                  | (src3 << 24);
-
-    int src_len;
-
-    for (src_len = rem; src_len > 0; src_len--)
-    {
-      const int idx = find_keyboard_layout_map (src, src_len, s_keyboard_layout_mapping, keyboard_layout_mapping_cnt);
-
-      if (idx == -1) continue;
-
-      u32 dst_char = s_keyboard_layout_mapping[idx].dst_char;
-      int dst_len  = s_keyboard_layout_mapping[idx].dst_len;
-
-      switch (dst_len)
-      {
-        case 1:
-          out_ptr[out_len++] = (dst_char >>  0) & 0xff;
-          break;
-        case 2:
-          out_ptr[out_len++] = (dst_char >>  0) & 0xff;
-          out_ptr[out_len++] = (dst_char >>  8) & 0xff;
-          break;
-        case 3:
-          out_ptr[out_len++] = (dst_char >>  0) & 0xff;
-          out_ptr[out_len++] = (dst_char >>  8) & 0xff;
-          out_ptr[out_len++] = (dst_char >> 16) & 0xff;
-          break;
-        case 4:
-          out_ptr[out_len++] = (dst_char >>  0) & 0xff;
-          out_ptr[out_len++] = (dst_char >>  8) & 0xff;
-          out_ptr[out_len++] = (dst_char >> 16) & 0xff;
-          out_ptr[out_len++] = (dst_char >> 24) & 0xff;
-          break;
-      }
-
-      plain_pos += src_len;
-
-      break;
-    }
-
-    // not matched, keep original
-
-    if (src_len == 0)
-    {
-      out_ptr[out_len] = plain_ptr[plain_pos];
-
-      out_len++;
-
-      plain_pos++;
-    }
-  }
-
-  plain_buf[ 0] = out_buf[ 0];
-  plain_buf[ 1] = out_buf[ 1];
-  plain_buf[ 2] = out_buf[ 2];
-  plain_buf[ 3] = out_buf[ 3];
-  plain_buf[ 4] = out_buf[ 4];
-  plain_buf[ 5] = out_buf[ 5];
-  plain_buf[ 6] = out_buf[ 6];
-  plain_buf[ 7] = out_buf[ 7];
-  plain_buf[ 8] = out_buf[ 8];
-  plain_buf[ 9] = out_buf[ 9];
-  plain_buf[10] = out_buf[10];
-  plain_buf[11] = out_buf[11];
-  plain_buf[12] = out_buf[12];
-  plain_buf[13] = out_buf[13];
-  plain_buf[14] = out_buf[14];
-  plain_buf[15] = out_buf[15];
-
-  return out_len;
-}
-
-/**
  * parsing
  */
 
@@ -244,6 +32,11 @@ int ascii_digest (const hashconfig_t *hashconfig, const hashes_t *hashes, const 
   const u32 dgst_size         = hashconfig->dgst_size;
   const u64 esalt_size        = hashconfig->esalt_size;
   const u64 hook_salt_size    = hashconfig->hook_salt_size;
+
+  if (hashconfig->opts_type & OPTS_TYPE_BINARY_HASHFILE)
+  {
+    return snprintf (out_buf, out_size, "%s", hashfile);
+  }
 
   //const u32 hash_type  = hashconfig->hash_type;
   //const u32 hash_mode  = hashconfig->hash_mode;

commit 4af26080b817b8db88553e4000ae7e792f74a9eb

Fix incorrect comparison result in sort_by_src_len()
The original sort_by_src_len() function returned 0 even when a > b,
violating the C standard requirements for qsort() comparison functions.
Specifically, it broke antisymmetry and transitivity, which can result
in undefined behavior.

In some versions of glibc, this leads not only to incorrect sorting but
also potential memory corruption[1].

Fix the issue by returning -1 when a > b, restoring compliance with the
standard.

Link: https://www.qualys.com/2024/01/30/qsort.txt [1]
Fixes: 8eb2558a7 ("Add -m 6211 module and moved some code around")

src/keyboard_layout.c
@@ -15,7 +15,10 @@ static int sort_by_src_len (const void *p1, const void *p2)
   const keyboard_layout_mapping_t *k1 = (const keyboard_layout_mapping_t *) p1;
   const keyboard_layout_mapping_t *k2 = (const keyboard_layout_mapping_t *) p2;
 
-  return k1->src_len < k2->src_len;
+  if (k1->src_len < k2->src_len) return 1;
+  if (k1->src_len > k2->src_len) return -1;
+
+  return 0;
 }
 
 bool initialize_keyboard_layout_mapping (const char *filename, keyboard_layout_mapping_t *keyboard_layout_mapping, int *keyboard_layout_mapping_cnt)