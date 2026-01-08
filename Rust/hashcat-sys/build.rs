59aff8918efadcde9b7e6fe6ee2babeb4d23d689
justpretending <justpretending@github.com>
2026-01-08 11:35:23 +0700

    Don't regen bindings.rs every time

--- Rust/hashcat-sys/build.rs
+++ Rust/hashcat-sys/build.rs
@@ -1,8 +1,12 @@
+use std::env;
+
 fn main() {
-    bindgen::Builder::default()
-        .header("src/hashcat_types.h")
-        .generate()
-        .expect("Unable to generate bindings")
-        .write_to_file("src/bindings.rs")
-        .expect("Couldn't write bindings!");
+    if env::var("REGENERATE_BINDINGS").is_ok() {
+        bindgen::Builder::default()
+            .header("src/hashcat_types.h")
+            .generate()
+            .expect("Unable to generate bindings")
+            .write_to_file("src/bindings.rs")
+            .expect("Couldn't write bindings!");
+    }
 }
