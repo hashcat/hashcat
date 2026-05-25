0574d3379247933be951e4c93d9ddfe8fac3c346
Chick3nman <admin@chick3nman.com>
2026-05-25 05:31:16 -0500

    Fix overflow in PKZIP modules

--- src/modules/module_17220.c
+++ src/modules/module_17220.c
@@ -296,6 +296,8 @@ int module_hash_decode (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSE
     if (p == NULL) return PARSER_HASH_LENGTH;
     pkzip->hashes[i].data_length = strtoul (p, NULL, 16);
 
+    if (pkzip->hashes[i].data_length > MAX_DATA) return (PARSER_TOKEN_LENGTH);
+
     p = strtok_r (NULL, "*", &saveptr);
     if (p == NULL) return PARSER_HASH_LENGTH;
     u16 checksum_from_crc = 0;
@@ -318,7 +320,12 @@ int module_hash_decode (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSE
     p = strtok_r (NULL, "*", &saveptr);
     if (p == NULL) return PARSER_HASH_LENGTH;
 
-    hex_to_binary (p, strlen (p), (char *) &(pkzip->hashes[i].data));
+    const size_t data_hex_len = strlen (p);
+
+    if (data_hex_len > MAX_DATA * 2) return (PARSER_TOKEN_LENGTH);
+    if (data_hex_len % 2)            return (PARSER_TOKEN_LENGTH);
+
+    hex_to_binary (p, data_hex_len, (char *) &(pkzip->hashes[i].data));
 
     // fake salt
     salt->salt_buf[i] = pkzip->hashes[i].data[0];
