4107938e3df39493233b4fae5ecb0c8434f544f3
Chick3nman <admin@chick3nman.com>
2026-05-29 13:43:32 -0500

    Fix module 32200

--- src/modules/module_32200.c
+++ src/modules/module_32200.c
@@ -320,7 +320,7 @@ int module_hash_encode (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSE
 {
   const krb5asrep_18_t *krb5asrep = (const krb5asrep_18_t *) esalt_buf;
 
-  char *data = (char *) hcmalloc (5120 * 4 * 2);
+  char *data = (char *) hcmalloc (5120 * 4 * 2 + 1);
 
   for (u32 i = 0, j = 0; i < krb5asrep->edata2_len; i += 1, j += 2)
   {
