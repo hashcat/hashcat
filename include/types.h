355f2f949797302fd3fdbfac748b940031e7f6fe
Gabriele Gristina <matrix@users.noreply.github.com>
2026-08-05 14:53:38 +0200

    speed up sorting/dedup of large non-salted hash lists with an in-place radix sort

--- include/types.h
+++ include/types.h
@@ -1115,6 +1115,9 @@ typedef struct hashes
 
   int          parser_token_length_cnt;
 
+  bool         radix_deduped;
+  bool         radix_digests_reordered;
+
 } hashes_t;
 
 typedef struct hashconfig
