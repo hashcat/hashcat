8b71261196b37883c8ce3b111a90287b183bd8d3
ajuneja <arjun.juneja@xtxmarkets.com>
2026-09-17 09:26:03 +0100

    fix: decode UTF-8 in m36500 test module to match pure kernels

--- tools/test_modules/m36500.pm
+++ tools/test_modules/m36500.pm
@@ -11,13 +11,20 @@ use warnings;
 use Encode;
 use Digest::MD4 qw (md4 md4_hex);
 
+my $PW_CHARSET = "latin1";
+
+if (exists $ENV{"IS_OPTIMIZED"} && defined $ENV{"IS_OPTIMIZED"} && $ENV{"IS_OPTIMIZED"} == 0)
+{
+  $PW_CHARSET = "utf-8";
+}
+
 sub module_constraints { [[0, 256], [-1, -1], [0, 27], [-1, -1], [-1, -1]] }
 
 sub module_generate_hash
 {
   my $word = shift;
 
-  my $inner = md4 (encode ("UTF-16LE", $word));
+  my $inner = md4 (encode ("UTF-16LE", decode ($PW_CHARSET, $word)));
   my $hash  = unpack ("H*", md4 (encode ("UTF-16LE", $inner)));
 
   return $hash;
